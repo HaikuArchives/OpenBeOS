012d17ad575cdcca09eadf7c17ccbb91cb37fcd2
robmed <>
2001-10-26 01:14:20 +0000

    Another change to the file header.

--- game_kit/source/lib/extended/headers/Point.h
+++ game_kit/source/lib/extended/headers/Point.h
@@ -1,14 +1,31 @@
-/*************************************************/
+/*****************************************************************************/
 // Point.h
 //
-// An open reimplementation of the Point class.
+// This is our generic implementation of the BPoint class. We need the
+// genericity to avoid writing a separate class for points with integer
+// coordinates.
 //
-// Written by:
-// 	Robert Medeiros
-// 	e-mail: robert.medeiros@sympatico.ca
 //
-// (C)2001 Robert Medeiros.
-/*************************************************/
+// Copyright (c) 2001 OpenBeOS Project
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included 
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+/*****************************************************************************/
 
 #ifndef EXTENDED_GAME_KIT_POINT
 #define EXTENDED_GAME_KIT_POINT
