22267670873749b8f776862c5f53b464936f9587
beveloper <>
2002-01-11 16:53:12 +0000

    added some BSoundPlayer functionality and supplemental files

--- media_kit/source/lib/SoundPlayer.cpp
+++ media_kit/source/lib/SoundPlayer.cpp
@@ -10,17 +10,19 @@
  * public sound_error
  *************************************************************/
 
+//final
 sound_error::sound_error(const char *str)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	m_str_const = str;
 }
 
-
+//final
 const char *
 sound_error::what() const
 {
-	UNIMPLEMENTED();
-	return NULL;
+	CALLED();
+	return m_str_const;
 }
 
 /*************************************************************
@@ -32,7 +34,15 @@ BSoundPlayer::BSoundPlayer(const char * name,
 						   void (*Notifier)(void *, sound_player_notification what, ...),
 						   void * cookie)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	media_multi_audio_format fmt = media_multi_audio_format::wildcard;
+	fmt.frame_rate = 44100.0f;
+	fmt.channel_count = 2;
+	fmt.format = media_raw_audio_format::B_AUDIO_FLOAT;
+	fmt.byte_order = B_MEDIA_HOST_ENDIAN;
+	//fmt.raw.buffer_size = 4096;
+
+	Init(NULL,&fmt,name,NULL,PlayBuffer,Notifier,cookie);
 }
 
 BSoundPlayer::BSoundPlayer(const media_raw_audio_format * format, 
@@ -41,7 +51,10 @@ BSoundPlayer::BSoundPlayer(const media_raw_audio_format * format,
 						   void (*Notifier)(void *, sound_player_notification what, ...),
 						   void * cookie)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	media_multi_audio_format fmt = media_multi_audio_format::wildcard;
+	memcpy(&fmt,format,sizeof(*format));
+	Init(NULL,&fmt,name,NULL,PlayBuffer,Notifier,cookie);
 }
 
 BSoundPlayer::BSoundPlayer(const media_node & toNode,
@@ -52,7 +65,10 @@ BSoundPlayer::BSoundPlayer(const media_node & toNode,
 						   void (*Notifier)(void *, sound_player_notification what, ...),
 						   void * cookie)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	if (toNode.kind & B_BUFFER_CONSUMER == 0)
+		debugger("BSoundPlayer: toNode must have B_BUFFER_CONSUMER kind!\n");
+	Init(&toNode,format,name,input,PlayBuffer,Notifier,cookie);
 }
 
 /* virtual */
@@ -65,9 +81,8 @@ BSoundPlayer::~BSoundPlayer()
 status_t
 BSoundPlayer::InitCheck()
 {
-	UNIMPLEMENTED();
-
-	return B_OK;
+	CALLED();
+	return _m_init_err;
 }
 
 
@@ -98,56 +113,62 @@ BSoundPlayer::Stop(bool block,
 	UNIMPLEMENTED();
 }
 
-
 BSoundPlayer::BufferPlayerFunc
 BSoundPlayer::BufferPlayer() const
 {
-	UNIMPLEMENTED();
-	BufferPlayerFunc dummy;
-
-	return dummy;
+	CALLED();
+	return _PlayBuffer;
 }
 
 void BSoundPlayer::SetBufferPlayer(void (*PlayBuffer)(void *, void * buffer, size_t size, const media_raw_audio_format & format))
 {
-	UNIMPLEMENTED();
+	CALLED();
+	_m_lock.Lock();
+	_PlayBuffer = PlayBuffer;
+	_m_lock.Unlock();
 }
-
 
 BSoundPlayer::EventNotifierFunc
 BSoundPlayer::EventNotifier() const
 {
-	UNIMPLEMENTED();
-	EventNotifierFunc dummy;
-
-	return dummy;
+	CALLED();
+	return _Notifier;
 }
 
 void BSoundPlayer::SetNotifier(void (*Notifier)(void *, sound_player_notification what, ...))
 {
-	UNIMPLEMENTED();
+	CALLED();
+	_m_lock.Lock();
+	_Notifier = Notifier;
+	_m_lock.Unlock();
 }
-
 
 void *
 BSoundPlayer::Cookie() const
 {
-	UNIMPLEMENTED();
-	return NULL;
+	CALLED();
+	return _m_cookie;
 }
-
 
 void
 BSoundPlayer::SetCookie(void *cookie)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	_m_lock.Lock();
+	_m_cookie = cookie;
+	_m_lock.Unlock();
 }
 
 void BSoundPlayer::SetCallbacks(void (*PlayBuffer)(void *, void * buffer, size_t size, const media_raw_audio_format & format),
 								void (*Notifier)(void *, sound_player_notification what, ...),
 								void * cookie)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	_m_lock.Lock();
+	SetBufferPlayer(PlayBuffer);
+	SetNotifier(Notifier);
+	SetCookie(cookie);
+	_m_lock.Unlock();
 }
 
 
@@ -295,10 +316,8 @@ BSoundPlayer::GetVolumeInfo(media_node *out_node,
 bigtime_t
 BSoundPlayer::Latency()
 {
-	UNIMPLEMENTED();
-	bigtime_t dummy;
-
-	return dummy;
+	BROKEN();
+	return 50000;
 }
 
 
@@ -323,10 +342,12 @@ BSoundPlayer::SetHasData(bool has_data)
  * protected BSoundPlayer
  *************************************************************/
 
+//final
 void
 BSoundPlayer::SetInitError(status_t in_error)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	_m_init_err = in_error;
 }
 
 
@@ -358,12 +379,93 @@ BSoundPlayer::get_volume_slider()
 	UNIMPLEMENTED();
 }
 
+void 
+BSoundPlayer::Init(
+					const media_node * node,
+					const media_multi_audio_format * format, 
+					const char * name,
+					const media_input * input,
+					void (*PlayBuffer)(void *, void * buffer, size_t size, const media_raw_audio_format & format),
+					void (*Notifier)(void *, sound_player_notification what, ...),
+					void * cookie)
+{
+	BROKEN();
+	_m_node = NULL;
+	_m_sounds = NULL;
+	_m_waiting = NULL;
+	_PlayBuffer = PlayBuffer;
+	_Notifier = Notifier;
+	//_m_lock;
+	_m_volume = 0.0f;
+	//m_input;
+	//m_output;
+	_m_mix_buffer = 0;
+	_m_mix_buffer_size = 0;
+	_m_cookie = cookie;
+	_m_buf = NULL;
+	_m_bufsize = 0;
+	_m_has_data = 0;
+	_m_init_err = B_ERROR;
+	_m_perfTime = 0;
+	_m_volumeSlider = NULL;
+	_m_gotVolume = 0;
+
+	_m_node = new _SoundPlayNode();
+	
+	status_t status; 
+	BMediaRoster *roster;
+	media_node outnode;
+	roster = BMediaRoster::Roster();	
+	if (!roster) {
+		TRACE("BSoundPlayer::Init: Couldn't get BMediaRoster\n");
+		return;
+	}
+	
+	//connect our producer node either to the 
+	//system mixer or to the supplied out node
+	if (!node) {
+		status = roster->GetAudioMixer(&outnode);
+		if (status != B_OK) {
+			TRACE("BSoundPlayer::Init: Couldn't GetAudioMixer\n");
+			SetInitError(status);
+			return;
+		}
+		node = &outnode;
+	}
+
+	m_input = ;
+	m_output = ;
+
+	
+tryFormat = fileAudioOutput.format; 
+   err = roster->Connect(fileAudioOutput.source, audioInput.destination, 
+               &tryFormat, &m_output, &m_input); 
+
+
+ err = roster->GetStartLatencyFor(timeSourceNode, &startTime); 
+   startTime += b_timesource->PerformanceTimeFor(BTimeSource::RealTime() 
+               + 1000000 / 50); 
+    
+   err = roster->StartNode(mediaFileNode, startTime); 
+   err = roster->StartNode(codecNode, startTime); 
+   err = roster->StartNode(videoNode, startTime); 
+
+	
+	
+	SetInitError(B_OK);
+}
 
 /* virtual */ void
 BSoundPlayer::Notify(sound_player_notification what,
 					 ...)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	_m_lock.Lock();
+	if (_Notifier)
+		(*_Notifier)(_m_cookie,what);
+	else {
+	}
+	_m_lock.Unlock();
 }
 
 
@@ -372,7 +474,13 @@ BSoundPlayer::PlayBuffer(void *buffer,
 						 size_t size,
 						 const media_raw_audio_format &format)
 {
-	UNIMPLEMENTED();
+	CALLED();
+	_m_lock.Lock();
+	if (_PlayBuffer)
+		(*_PlayBuffer)(_m_cookie,buffer,size,format);
+	else {
+	}
+	_m_lock.Unlock();
 }
 
 
