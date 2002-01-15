e6247b0a7f2aa2fe0dc24dab3bf1f78496958c50
freyr <>
2002-01-15 12:23:35 +0000

    Commit of the net_kit library skeleton.  Should compile given the BeOS headers.
    Haven't committed the headers because I'm looking for clarification
    on copyright issues, check openbeosnetteam@freelists.org list.

---
+++ net_kit/source/lib/NetAddress.cpp
@@ -0,0 +1,91 @@
+#include <Archivable.h>
+
+#include "NetAddress.h"
+
+BNetAddress::BNetAddress(BMessage *archive) {
+}
+
+BNetAddress::~BNetAddress() {
+}
+
+status_t BNetAddress::Archive(BMessage *into, bool deep = true) const {
+}
+
+BArchivable *BNetAddress::Instantiate(BMessage *archive) {
+}
+ 
+BNetAddress::BNetAddress(const char *hostname = 0, unsigned short port = 0) {
+}
+
+BNetAddress::BNetAddress(const struct sockaddr_in &sa) {
+}
+
+BNetAddress::BNetAddress(in_addr addr, int port = 0) {
+}
+
+BNetAddress::BNetAddress(uint32 addr, int port = 0 ) {
+}
+
+BNetAddress::BNetAddress(const BNetAddress &) {
+}
+
+BNetAddress::BNetAddress(const char *hostname, const char *protocol, const char *service) {
+}
+
+BNetAddress &BNetAddress::operator=(const BNetAddress &) {
+}
+
+status_t BNetAddress::InitCheck() {
+	return B_ERROR;
+}
+
+status_t BNetAddress::SetTo(const char *hostname, const char *protocol, const char *service) {
+	return B_ERROR;
+}
+
+status_t BNetAddress::SetTo(const char *hostname = 0, unsigned short port = 0) {
+	return B_ERROR;
+}
+
+status_t BNetAddress::SetTo(const struct sockaddr_in &sa) {
+	return B_ERROR;
+}
+
+status_t BNetAddress::SetTo(in_addr addr, int port = 0) {
+	return B_ERROR;
+}
+
+status_t BNetAddress::SetTo(uint32 addr=INADDR_ANY, int port = 0) {
+	return B_ERROR;
+}
+
+status_t BNetAddress::GetAddr(char *hostname = 0, unsigned short *port = 0) const {
+	return B_ERROR;
+}
+
+status_t BNetAddress::GetAddr(struct sockaddr_in &sa) const {
+	return B_ERROR;
+}
+
+status_t BNetAddress::GetAddr(in_addr &addr, unsigned short *port = 0) const {
+	return B_ERROR;
+}
+
+void BNetAddress::_ReservedBNetAddressFBCCruft1() {
+}
+
+void BNetAddress::_ReservedBNetAddressFBCCruft2() {
+}
+
+void BNetAddress::_ReservedBNetAddressFBCCruft3() {
+}
+
+void BNetAddress::_ReservedBNetAddressFBCCruft4() {
+}
+
+void BNetAddress::_ReservedBNetAddressFBCCruft5() {
+}
+
+void BNetAddress::_ReservedBNetAddressFBCCruft6() {
+}
+
