eadae2e435f88b23ab45644167248491087d9d04
k3079e6 <>
2002-01-28 14:39:15 +0000

    Initial checkin from Sikosis.

---
+++ preferences/DUN/DUNView.h
@@ -0,0 +1,18 @@
+/*
+
+DUNView by Sikosis
+
+(C) 2002
+
+*/
+
+#ifndef __DUNVIEW_H__
+#define __DUNVIEW_H__
+
+class DUNView : public BView {
+public:
+   DUNView(BRect frame);
+   virtual void Draw (BRect updateRect);
+};
+
+#endif
