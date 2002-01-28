b742bf092afaef79862c04830e04c9ea88bda382
k3079e6 <>
2002-01-28 14:23:50 +0000

    Removed all files.

--- preferences/DUN/screensaver_kit/source/server/SSAwindow.h
+++
@@ -1,9 +0,0 @@
-#include "DirectWindow.h"
-
-class SSAwindow : public BDirectWindow {
-public:
-	SSAwindow(BRect frame);
-	~SSAwindow();
-	virtual bool QuitRequested();
-	virtual void DirectConnected(direct_buffer_info *info);
-};
