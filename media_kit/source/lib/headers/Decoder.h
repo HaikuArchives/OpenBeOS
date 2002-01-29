d2496159b61e6a3fce2a57989cfa7e2b37f96dae
beveloper <>
2002-01-29 01:41:31 +0000

    we are going to create our own media codec api

--- media_kit/source/lib/headers/Decoder.h
+++ media_kit/source/lib/headers/Decoder.h
@@ -1,12 +1,15 @@
 
+// this header is similar to functions found in libmedia.so 
+// and the media_kit decoder add-ons
+// we will use it as basic information, but are not trying to
+// create a binary compatible interface
+// this API will totally change, as we are creating our own API
+
 #include <MediaDefs.h>
 #include <MediaFormats.h>
 
-namespace BPrivate
-{
+namespace media_kit_private {
 
-
-/* functions parameteres similar to MediaDecoder.h if possible, returntype status_t assumed */
 class Decoder
 {
 public:
@@ -20,7 +23,6 @@ public:
 	virtual status_t GetNextChunk(void const **chunkData, size_t *chunkSize, 
 								  media_header *mh, media_decode_info *mdi);
 
-	/* these 4 functions do not appear in libmedia.so, they must be pure virtual */
 	virtual status_t Decode(void *out_buffer, int64 *out_frameCount,
 							media_header *out_mh, media_decode_info *info) = 0;
 	virtual status_t Format(media_format *) = 0;
@@ -28,71 +30,13 @@ public:
 	virtual status_t GetCodecInfo(media_codec_info *out_mci) const = 0;
 
 private:
-
-	// MediaDecoder.h has 32 here, may be more or less
-	//uint32 _reserved_data[32]; 
-	//we also may have to add some local variables, 
-	//and if the used bool, char, or uint16 it's not a multiple of 4
-	//let's make a guess. 32 * uint32 + 2 bool + 2 pointers would be 138
-	//figuring out the exact size/layout is left as an exercise for the reader
-	uint8 reserved[42]; //but if 42 is the answer, what's the question
-
 	virtual status_t Perform(long, void *);
-	
-	//probably some are already used, so they are no longer virtual
-	virtual status_t _Reserved_Decoder_0(long,...);
-	virtual status_t _Reserved_Decoder_1(long,...);
-	virtual status_t _Reserved_Decoder_2(long,...);
-	virtual status_t _Reserved_Decoder_3(long,...);
-	virtual status_t _Reserved_Decoder_4(long,...);
-	virtual status_t _Reserved_Decoder_5(long,...);
-	virtual status_t _Reserved_Decoder_6(long,...);
-	virtual status_t _Reserved_Decoder_7(long,...);
-	virtual status_t _Reserved_Decoder_8(long,...);
-	virtual status_t _Reserved_Decoder_9(long,...);
-	virtual status_t _Reserved_Decoder_10(long,...);
-	virtual status_t _Reserved_Decoder_11(long,...);
-	virtual status_t _Reserved_Decoder_12(long,...);
-	virtual status_t _Reserved_Decoder_13(long,...);
-	virtual status_t _Reserved_Decoder_14(long,...);
-	virtual status_t _Reserved_Decoder_15(long,...);
-	virtual status_t _Reserved_Decoder_16(long,...);
-	virtual status_t _Reserved_Decoder_17(long,...);
-	virtual status_t _Reserved_Decoder_18(long,...);
-	virtual status_t _Reserved_Decoder_19(long,...);
-	virtual status_t _Reserved_Decoder_20(long,...);
-	virtual status_t _Reserved_Decoder_21(long,...);
-	virtual status_t _Reserved_Decoder_22(long,...);
-	virtual status_t _Reserved_Decoder_23(long,...);
-	virtual status_t _Reserved_Decoder_24(long,...);
-	virtual status_t _Reserved_Decoder_25(long,...);
-	virtual status_t _Reserved_Decoder_26(long,...);
-	virtual status_t _Reserved_Decoder_27(long,...);
-	virtual status_t _Reserved_Decoder_28(long,...);
-	virtual status_t _Reserved_Decoder_29(long,...);
-	virtual status_t _Reserved_Decoder_30(long,...);
-	virtual status_t _Reserved_Decoder_31(long,...);
-	virtual status_t _Reserved_Decoder_32(long,...);
-	virtual status_t _Reserved_Decoder_33(long,...);
-	virtual status_t _Reserved_Decoder_34(long,...);
-	virtual status_t _Reserved_Decoder_35(long,...);
-	virtual status_t _Reserved_Decoder_36(long,...);
-	virtual status_t _Reserved_Decoder_37(long,...);
-	virtual status_t _Reserved_Decoder_38(long,...);
-	virtual status_t _Reserved_Decoder_39(long,...);
-	virtual status_t _Reserved_Decoder_40(long,...);
-	virtual status_t _Reserved_Decoder_41(long,...);
-	virtual status_t _Reserved_Decoder_42(long,...);
-	virtual status_t _Reserved_Decoder_43(long,...);
-	virtual status_t _Reserved_Decoder_44(long,...);
-	virtual status_t _Reserved_Decoder_45(long,...);
-	virtual status_t _Reserved_Decoder_46(long,...);
-	virtual status_t _Reserved_Decoder_47(long,...);
+	//virtual function and data stuffing will be done later
 };
 
-}
+}; // namespace media_kit_private
 
-//any parameters? or none at all?
-extern "C" status_t register_decoder(int32 p1,int32 p2,int32 p3,int32 p4,int32 p5,int32 p6,int32 p7,int32 p8);
-extern "C" BPrivate::Decoder * instantiate_decoder(int32 p1,int32 p2,int32 p3,int32 p4,int32 p5,int32 p6,int32 p7,int32 p8);
+
+extern "C" status_t register_decoder();
+extern "C" BPrivate::Decoder * instantiate_decoder();
 
--- media_kit/source/lib/headers/Encoder.h
+++ media_kit/source/lib/headers/Encoder.h
@@ -1,10 +1,12 @@
-#include <MediaDefs.h>
-#include <MediaFormats.h>
 
-namespace BPrivate
-{
+// this header is similar to functions found in libmedia.so 
+// and the media_kit encoder add-ons
+// we will use it as basic information, but are not trying to
+// create a binary compatible interface
+// this API will totally change, as we are creating our own API
 
-/* functions parameteres similar to MediaEncoder.h if possible, returntype status_t assumed */
+namespace media_kit_private {
+
 class Encoder
 {
 public:
@@ -26,77 +28,17 @@ public:
 	virtual status_t AttachedToTrack(void);
 	virtual status_t SetTrack(BMediaTrack *);
 	
-	/* these 3 functions do not appear in libmedia.so, they must be pure virtual */
 	virtual status_t GetCodecInfo(media_codec_info *) const = 0;
 	virtual status_t Encode(void const *, long long, media_encode_info *) = 0;
 	virtual status_t SetFormat(media_file_format *, media_format *, media_format *) = 0;
 
 private:
-	// MediaEncoder.h has 32 here, may be more or less
-	//uint32 _reserved_data[32]; 
-	//we also may have to add some local variables, 
-	//and if the used bool, char, or uint16 it's not a multiple of 4
-	//let's make a guess. 32 * uint32 + 2 bool + 2 pointers would be 138
-	//figuring out the exact size/layout is left as an exercise for the reader
-	uint8 reserved[42]; //but if 42 is the answer, what's the question
-	
 	virtual status_t Perform(long, void *);
-
-	//probably some are already used, so they are no longer virtual
-	virtual status_t _Reserved_Encoder_0(long,...);
-	virtual status_t _Reserved_Encoder_1(long,...);
-	virtual status_t _Reserved_Encoder_2(long,...);
-	virtual status_t _Reserved_Encoder_3(long,...);
-	virtual status_t _Reserved_Encoder_4(long,...);
-	virtual status_t _Reserved_Encoder_5(long,...);
-	virtual status_t _Reserved_Encoder_6(long,...);
-	virtual status_t _Reserved_Encoder_7(long,...);
-	virtual status_t _Reserved_Encoder_8(long,...);
-	virtual status_t _Reserved_Encoder_9(long,...);
-	virtual status_t _Reserved_Encoder_10(long,...);
-	virtual status_t _Reserved_Encoder_11(long,...);
-	virtual status_t _Reserved_Encoder_12(long,...);
-	virtual status_t _Reserved_Encoder_13(long,...);
-	virtual status_t _Reserved_Encoder_14(long,...);
-	virtual status_t _Reserved_Encoder_15(long,...);
-	virtual status_t _Reserved_Encoder_16(long,...);
-	virtual status_t _Reserved_Encoder_17(long,...);
-	virtual status_t _Reserved_Encoder_18(long,...);
-	virtual status_t _Reserved_Encoder_19(long,...);
-	virtual status_t _Reserved_Encoder_20(long,...);
-	virtual status_t _Reserved_Encoder_21(long,...);
-	virtual status_t _Reserved_Encoder_22(long,...);
-	virtual status_t _Reserved_Encoder_23(long,...);
-	virtual status_t _Reserved_Encoder_24(long,...);
-	virtual status_t _Reserved_Encoder_25(long,...);
-	virtual status_t _Reserved_Encoder_26(long,...);
-	virtual status_t _Reserved_Encoder_27(long,...);
-	virtual status_t _Reserved_Encoder_28(long,...);
-	virtual status_t _Reserved_Encoder_29(long,...);
-	virtual status_t _Reserved_Encoder_30(long,...);
-	virtual status_t _Reserved_Encoder_31(long,...);
-	virtual status_t _Reserved_Encoder_32(long,...);
-	virtual status_t _Reserved_Encoder_33(long,...);
-	virtual status_t _Reserved_Encoder_34(long,...);
-	virtual status_t _Reserved_Encoder_35(long,...);
-	virtual status_t _Reserved_Encoder_36(long,...);
-	virtual status_t _Reserved_Encoder_37(long,...);
-	virtual status_t _Reserved_Encoder_38(long,...);
-	virtual status_t _Reserved_Encoder_39(long,...);
-	virtual status_t _Reserved_Encoder_40(long,...);
-	virtual status_t _Reserved_Encoder_41(long,...);
-	virtual status_t _Reserved_Encoder_42(long,...);
-	virtual status_t _Reserved_Encoder_43(long,...);
-	virtual status_t _Reserved_Encoder_44(long,...);
-	virtual status_t _Reserved_Encoder_45(long,...);
-	virtual status_t _Reserved_Encoder_46(long,...);
-	virtual status_t _Reserved_Encoder_47(long,...);
+	//virtual function and data stuffing will be done later
 };
 
-}
+}; // namespace media_kit_private
 
-// any parameters? or none at all?
-// and what does _nth_encoder refer to? why nth?
-extern "C" BPrivate::Encoder *instantiate_nth_encoder(int32 p1,int32 p2,int32 p3,int32 p4,int32 p5);
-extern "C" status_t register_encoder(int32 p1,int32 p2,int32 p3,int32 p4,int32 p5,int32 p6,int32 p7,int32 p8);
-extern "C" BPrivate::Encoder *instantiate_encoder(int32 p1,int32 p2,int32 p3,int32 p4,int32 p5,int32 p6,int32 p7,int32 p8);
+extern "C" status_t register_encoder();
+extern "C" BPrivate::Encoder *instantiate_nth_encoder();
+extern "C" BPrivate::Encoder *instantiate_encoder();
