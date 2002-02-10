e75e49301b59915799bb093008817c800f14a0b2
dreid <>
2002-02-10 00:04:10 +0000

    Some improvements for IPv4...
    - fix the header definitions (allow for endianness)
    - add udp hack

--- net_kit/source/server/ipv4/ipv4.c
+++ net_kit/source/server/ipv4/ipv4.c
@@ -8,6 +8,9 @@
 #include "ipv4/ipv4.h"
 #include "protocols.h"
 
+/* hack to get the thing working... */
+#include "udp/udp.h"
+
 static void dump_ipv4_addr(char *msg, ipv4_addr *ad)
 {
 	uint8 *b = (uint8*)ad;
@@ -19,21 +22,29 @@ int ipv4_input(struct mbuf *buf)
 	ipv4_header *ip = mtod(buf, ipv4_header *);
 	
 	printf("IPv4 Header :\n");
-	printf("            : version       : %d\n", IPV4_VERSION(ip));
-	printf("            : header length : %d\n", IPV4_HDR_LENGTH(ip));
+	printf("            : version       : %d\n", ip->ver);
+	printf("            : header length : %d\n", ip->hl);
 	printf("            : tos           : %d\n", ip->tos);
 	printf("            : total length  : %d\n", ntohs(ip->length));
 	printf("            : id            : %d\n", ntohs(ip->id));
 	printf("            : flags         : 0x%02x\n", IPV4_FLAGS(ip));
 	printf("            : frag offset   : %d\n", ntohs(IPV4_FRAG(ip)));
 	printf("            : ttl           : %d\n", ip->ttl);
+        dump_ipv4_addr("            : src address   :", &ip->src);
+        dump_ipv4_addr("            : dst address   :", &ip->dst);
+
 	printf("            : protocol      : ");
+	/* move the pointer... */
+	/* Yuck - big hack - adding 14 isn't correct! */
+	m_adj(buf, (ip->hl * 4) + 14);
+
 	switch(ip->prot) {
 		case IP_ICMP:
 			printf("ICMP\n");
 			break;
 		case IP_UDP:
 			printf("UDP\n");
+			udp_input(buf);
 			break;
 		case IP_TCP:
 			printf("TCP\n");
@@ -41,8 +52,6 @@ int ipv4_input(struct mbuf *buf)
 		default:
 			printf("unknown (0x%02x)\n", ip->prot);
 	}
-	dump_ipv4_addr("            : src address   :", &ip->src);
-	dump_ipv4_addr("            : dst address   :", &ip->dst);
 
 	return 0; 
 }
--- net_kit/source/server/ipv4/ipv4.h
+++ net_kit/source/server/ipv4/ipv4.h
@@ -5,6 +5,8 @@
 #ifndef OBOS_IPV4_H
 #define OBOS_IPV4_H
 
+#include <ByteOrder.h>
+
 #include "mbuf.h"
 #include "net_misc.h"
 
@@ -15,7 +17,13 @@ enum {
 };
 
 typedef struct ipv4_header {
-	uint8	ver_ihl;
+#if B_HOST_IS_BENDIAN
+	uint8	ver:4;
+	uint8	hl:4;
+#else
+	uint8	hl:4;
+	uint8	ver:4;
+#endif
 	uint8	tos;
 	uint16	length;
 	uint16	id;
@@ -25,13 +33,10 @@ typedef struct ipv4_header {
 	uint16	hdr_cksum;
 	ipv4_addr	src;
 	ipv4_addr	dst;
-} ipv4_header;
+} _PACKED ipv4_header;
 
-#define IPV4_VERSION(a)	(a)->ver_ihl >> 4
-#define IPV4_HDR_LENGTH(a)	(a)->ver_ihl & 0x0e
-#define IPV4_HDR_BYTES(a)	IPV4_HDR_LENGTH(a) * 4
 #define IPV4_FLAGS(a)		(a)->flags_frag >> 13
-#define IPV4_FRAG(a)		(a)->flags_frag & 0x20ff
+#define IPV4_FRAG(a)		(a)->flags_frag & 0x1fff
 
 int ipv4_input(struct mbuf* buf);
 
