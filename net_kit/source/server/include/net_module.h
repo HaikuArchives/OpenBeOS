f268edd65e5a7165475d2925294c9a68838edb40
dreid <>
2002-02-10 23:45:23 +0000

    Add some files that I missed previously and remove testread as it no longer works and net_srv does a lot more anyways.

---
+++ net_kit/source/server/include/net_module.h
@@ -0,0 +1,43 @@
+/* net_module.h
+ * fundtions and staructures for all modules using the
+ * net_server
+ */
+
+#include <image.h>
+
+#include "mbuf.h"
+#include "if.h"
+
+#ifndef OBOS_NET_MODULE_H
+#define OBOS_NET_MODULE_H
+
+typedef struct loaded_net_module 	loaded_net_module;
+
+/* each net_module MUST define eaxctaly ONE of these, completely 
+ * filled in, and it MUST be called net_module_data
+ */
+typedef struct net_module {
+	char *name;
+	int proto;
+	int layer;
+
+	int (*init) (loaded_net_module *, int *);
+	int (*dev_init) (ifnet *);
+	int (*input) (struct mbuf *);
+	int (*output) (struct mbuf*);
+} net_module;
+
+struct loaded_net_module {
+        struct net_module *mod;
+        image_id iid;
+        int ref_count;
+};
+
+enum {
+	NET_LAYER1	= 1,
+	NET_LAYER2,
+	NET_LAYER3
+};
+
+#endif /* OBOS_NET_MODULE_H */
+
