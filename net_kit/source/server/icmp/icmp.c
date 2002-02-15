3d4dd28fb9b5ad866ceb2a8a6c97bfe88467a0bf
dreid <>
2002-02-15 23:35:46 +0000

    ICMP module. so far it'll reply very nicely to echo requests.

---
+++ net_kit/source/server/icmp/icmp.c
@@ -0,0 +1,102 @@
+/* icmp.c
+ */
+ 
+#include <stdio.h>
+
+#include "mbuf.h"
+#include "icmp.h"
+#include "ipv4/ipv4.h"
+#include "net_module.h"
+#include "protocols.h"
+#include "sys/socket.h"
+
+static loaded_net_module *global_modules;
+static int *prot_table;
+
+#if SHOW_DEBUG
+static void dump_icmp(struct mbuf *buf)
+{
+	ipv4_header *ip = mtod(buf, ipv4_header *);
+	icmp_header *ic =  (icmp_header*)((caddr_t)ip + (ip->hl * 4));
+
+        printf("ICMP: ");
+        switch (ic->type) {
+		case ICMP_ECHO_RQST:
+			printf ("Echo request\n");
+			break;
+		case ICMP_ECHO_RPLY:
+			printf("echo reply\n");
+			break;
+		default:
+			printf("?? type = %d\n", ic->type);
+	}
+}
+#endif
+
+int icmp_input(struct mbuf *buf)
+{
+	ipv4_header *ip = mtod(buf, ipv4_header *);	
+	icmp_header *ic;
+	int icl = ntohs(ip->length) - (ip->hl * 4);
+
+	ic = (icmp_header*)((caddr_t)ip + (ip->hl * 4));
+
+#if SHOW_DEBUG
+	dump_icmp(buf);
+#endif
+
+	if (in_cksum(buf, icl, (ip->hl * 4)) != 0) {
+		printf("Checksum failed!\n");
+		m_freem(buf);
+		return 0;
+	}	
+
+	switch (ic->type) {
+		case ICMP_ECHO_RQST: {
+			icmp_echo *ie = (icmp_echo *)ic;
+			struct sockaddr sa;
+			ie->hdr.type = ICMP_ECHO_RPLY;
+
+			sa.sa_family = AF_INET;
+			sa.sa_len = 4;
+			memcpy(&sa.sa_data, &ip->src, 4);
+
+			ie->hdr.cksum = 0;
+			ie->hdr.cksum = in_cksum(buf, icl, (ip->hl * 4));
+			ip->src = ip->dst;
+			memcpy(&ip->dst, &sa.sa_data, 4);
+			ip->hdr_cksum = 0;
+			ip->hdr_cksum = in_cksum(buf, (ip->hl * 4), 0);
+
+			global_modules[prot_table[NS_ETHER]].mod->output(buf, NS_IPV4, &sa);
+
+			return 0;
+			break;
+		}
+		case ICMP_ECHO_RPLY:
+			break;
+		default:
+	}
+	m_freem(buf);
+	return 0;
+}
+
+static int icmp_init(loaded_net_module *ln, int *pt)
+{
+	global_modules = ln;
+	prot_table = pt;
+	return 0;
+}
+
+net_module net_module_data = {
+	"ICMP Module",
+	NS_ICMP,
+	NET_LAYER2,
+
+	&icmp_init,
+	NULL,
+	&icmp_input,
+	NULL,
+	NULL
+};
+
---
+++ net_kit/source/server/icmp/icmp.h
@@ -0,0 +1,40 @@
+/* icmp.h
+ * icmp for IPv4
+ */
+
+#include "mbuf.h"
+#include "sys/socket.h"
+
+#ifndef OBOS_ICMP_H
+#define OBOS_ICMP_H
+
+enum {
+	ICMP_ECHO_RPLY 		= 0,
+	ICMP_DEST_UNRCH		= 3,
+	ICMP_SRC_QUENCH		= 4,
+	ICMP_REDIRECT		= 5,
+	ICMP_ECHO_RQST		= 8,
+	ICMP_RTR_ADVERT		= 9,
+	ICMP_RTR_SOL		= 10,
+	ICMP_TIME_UP		= 11,
+	ICMP_PARAM		= 12,
+	/* XXX - add the remaining ones... */
+};
+
+typedef struct icmp_header {
+	uint8	type;
+	uint8	code;
+	uint16	cksum;
+} _PACKED icmp_header;
+
+typedef struct icmp_echo {
+	icmp_header hdr;
+	uint16	id;
+	uint16	seq;
+} icmp_echo;
+
+int icmp_input(struct mbuf *buf);
+int icmp_output(struct mbuf *buf, struct sockaddr *sa);
+
+#endif /* OBOS_ICMP_H */
+
