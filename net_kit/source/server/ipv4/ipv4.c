35966f24c4e65825eef3eaf2766fd1b2f4985b86
dreid <>
2002-02-15 23:40:43 +0000

    OK, this will be the first of a few that move the debug print out of the main input function and then add a call to it. they're all hidden behind the SHOW_DEBUG #if (coming real soon now).

--- net_kit/source/server/ipv4/ipv4.c
+++ net_kit/source/server/ipv4/ipv4.c
@@ -15,46 +15,54 @@
 loaded_net_module *net_modules;
 int *prot_table;
 
+#if SHOW_DEBUG
+static void dump_ipv4_header(struct mbuf *buf)
+{
+	ipv4_header *ip = mtod(buf, ipv4_header *);
+
+        printf("IPv4 Header :\n");
+        printf("            : version       : %d\n", ip->ver);
+        printf("            : header length : %d\n", ip->hl);
+        printf("            : tos           : %d\n", ip->tos);
+        printf("            : total length  : %d\n", ntohs(ip->length));
+        printf("            : id            : %d\n", ntohs(ip->id));
+        printf("            : flags         : 0x%02x\n", IPV4_FLAGS(ip));
+        printf("            : frag offset   : %d\n", ntohs(IPV4_FRAG(ip)));
+        printf("            : ttl           : %d\n", ip->ttl);
+        dump_ipv4_addr("            : src address   :", &ip->src);
+        dump_ipv4_addr("            : dst address   :", &ip->dst);
+
+        printf("            : protocol      : ");
+
+        switch(ip->prot) {
+                case IP_ICMP:
+                        printf("ICMP\n");
+                        break;
+                case IP_UDP: {
+                        printf("UDP\n");
+                        break;
+                }
+                case IP_TCP:
+                        printf("TCP\n");
+                        break;
+                default:
+                        printf("unknown (0x%02x)\n", ip->prot);
+        }
+}
+#endif
+
 int ipv4_input(struct mbuf *buf)
 {
 	ipv4_header *ip = mtod(buf, ipv4_header *);
 
+#if SHOW_DEBUG
+	dump_ipv4_header(buf);
+#endif
 
-	if (in_cksum(buf, ip->hl * 4) != 0) {
+	if (in_cksum(buf, ip->hl * 4, 0) != 0) {
 		printf("Bogus checksum! Discarding packet.\n");
 		m_freem(buf);
 		return 0;
-	}
-
-	printf("IPv4 Header :\n");
-	printf("            : version       : %d\n", ip->ver);
-	printf("            : header length : %d\n", ip->hl);
-	printf("            : tos           : %d\n", ip->tos);
-	printf("            : total length  : %d\n", ntohs(ip->length));
-	printf("            : id            : %d\n", ntohs(ip->id));
-	printf("            : flags         : 0x%02x\n", IPV4_FLAGS(ip));
-	printf("            : frag offset   : %d\n", ntohs(IPV4_FRAG(ip)));
-	printf("            : ttl           : %d\n", ip->ttl);
-	dump_ipv4_addr("            : src address   :", &ip->src);
-	dump_ipv4_addr("            : dst address   :", &ip->dst);
-
-	printf("            : protocol      : ");
-	/* move the pointer... not sure if all protocols will want this! */
-	m_adj(buf, (ip->hl * 4));
-
-	switch(ip->prot) {
-		case IP_ICMP:
-			printf("ICMP\n");
-			break;
-		case IP_UDP: {
-			printf("UDP\n");
-			break;
-		}
-		case IP_TCP:
-			printf("TCP\n");
-			break;
-		default:
-			printf("unknown (0x%02x)\n", ip->prot);
 	}
 
 	if (net_modules[prot_table[ip->prot]].mod->input)
@@ -106,3 +114,4 @@ net_module net_module_data = {
 	NULL,
 	NULL
 };
+
