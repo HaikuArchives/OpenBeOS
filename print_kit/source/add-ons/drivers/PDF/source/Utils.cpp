4f04bd8e19afd1afba6c356346d8b58dc2119f81
laplace <>
2002-02-17 19:30:17 +0000

    Temporary remove Celerick from the hall of fame ;-)

--- print_kit/source/add-ons/drivers/PDF/source/Utils.cpp
+++ print_kit/source/add-ons/drivers/PDF/source/Utils.cpp
@@ -103,7 +103,8 @@ kAbout =
 "\tPhilippe Houdoin - Project Leader\n"
 "\tSimon Gauvin - GUI Design\n"
 "\tMichael Pfeiffer - PDF Generation\n"
-"\tCelerick Stephens - Documentation\n";
+//"\tCelerick Stephens - Documentation\n"
+;
 
 void
 HWindow::AboutRequested()
