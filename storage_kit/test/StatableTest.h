dd83c865bb5473b9666fdc7ec5b435b625d71a47
mrmlk <>
2002-03-08 07:31:22 +0000

    Started test for GetStat()

--- storage_kit/test/StatableTest.h
+++ storage_kit/test/StatableTest.h
@@ -1,5 +1,5 @@
-#ifndef __sk_path_test_h__
-#define __sk_path_test_h__
+#ifndef __sk_statable_test_h__
+#define __sk_statable_test_h__
 
 #include <cppunit/TestCase.h>
 #include <cppunit/TestCaller.h>
@@ -9,7 +9,7 @@
 #include <Entry.h>
 
 #define STATABLE BEntry
-// Also test with BNode
+// Also test with STATBLE set to BNode
 
 class StatableTest : public CppUnit::TestCase
 {
@@ -17,13 +17,12 @@ public:
 	static Test* Suite() {
 		CppUnit::TestSuite *suite = new CppUnit::TestSuite();
 		
-		suite->addTest( new CppUnit::TestCaller<PathTest>("BStatable::IsFile"(), &StatableTest::IsFile) );
+		suite->addTest( new CppUnit::TestCaller<PathTest>("BStatable::IsFile()", &StatableTest::IsFile) );
 
-		suite->addTest( new CppUnit::TestCaller<PathTest>("BStatable::IsLink"(), &StatableTest::IsLink) );
+		suite->addTest( new CppUnit::TestCaller<PathTest>("BStatable::IsLink()", &StatableTest::IsLink) );
 
-		suite->addTest( new CppUnit::TestCaller<PathTest>("BStatable::IsDirectory"(), &StatableTest::IsDirectory) );	  
-
-
+		suite->addTest( new CppUnit::TestCaller<PathTest>("BStatable::IsDirectory()", &StatableTest::IsDirectory) );	  
+		suite->addTest( new CppUnut::TestCaller<PathTest>("BStatable::GetStat()", &StatableTest::GetStat ) );
 		return suite;
 	}
 
@@ -63,15 +62,30 @@ public:
 		STATABLE file("/system/Tracker", false);
 		STATABLE f404("/what_ya_bet/some_one_has a file called this", false);
 
-		CPPUNIT_ASSERT( folder.IsLink() == false ); 
-		CPPUNIT_ASSERT( link.IsLink() == true );
-		CPPUNIT_ASSERT( file.IsLink == false );
-		CPPUNIT_ASSERT( f404.IsLink == false );
+		CPPUNIT_ASSERT( folder.IsSymLink() == false ); 
+		CPPUNIT_ASSERT( link.IsSymLink() == true );
+		CPPUNIT_ASSERT( file.IsSymLink() == false );
+		CPPUNIT_ASSERT( f404.IsSymLink() == false );
 	}
 
+	void GetStat() {
+  		STATABLE folder("/", false);
+		STATABLE link("/system/", false);
+		STATABLE file("/system/Tracker", false);
+		STATABLE f404("/what_ya_bet/some_one_has a file called this", false);
+		
+		struct stat folderStat;
+		struct stat linkStat;
+		struct stat fileStat;
+		struct stat f404Stat;
+		
+		CPPUNIT_ASSERT( folder.GetStat(&folderStat) == B_OK );
+		CPPUNIT_ASSERT( link.GetStat(&linkStat) == B_OK );
+		CPPUNIT_ASSERT( file.GetStat(&fileStat) == B_OK );
+		CPPUNIT_ASSERT( f404.GetStat(&f404Stat) == B_BAD_VALUE );
+
+	}
 	
 };
 
-
-
-#endif	// __sk_path_test_h__
+#endif	// __sk_statable_test_h__
