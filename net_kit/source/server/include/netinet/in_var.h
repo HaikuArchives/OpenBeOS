587e0b59825ea12980ed8aa87b3cb2be37a87f03
dreid <>
2002-03-11 23:40:14 +0000

    More BSD style header files.

---
+++ net_kit/source/server/include/netinet/in_var.h
@@ -0,0 +1,38 @@
+/* in_var.h */
+
+#include "netinet/in.h"
+
+#ifndef NETINET_IN_VAR_H
+#define NETINET_IN_VAR_H
+
+struct in_ifaddr {
+	struct ifaddr 		ia_ifa;
+	struct in_ifaddr 	*ia_next;
+
+	uint32			ia_net;
+	uint32			ia_netmask;
+	uint32			ia_subnet;
+	uint32			ia_subnetmask;
+	struct in_addr 		ia_netbroadcast;
+	struct sockaddr_in 	ia_addr;
+	struct sockaddr_in 	ia_dstaddr;		/* broadcast address */
+	struct sockaddr_in	ia_sockmask;
+	/*XXX - milticast address list */
+};
+#define ia_ifp		ia_ifa.ifn
+#define ia_flags	ia_ifa.ifa_flags
+#define ia_broadaddr	ia_dstaddr
+
+#define ifatoia(ifa)    ((struct in_ifaddr *)(ifa))
+#define sintosa(sin)	((struct sockaddr *)(sin))
+
+/*
+ * Given a pointer to an in_ifaddr (ifaddr),
+ * return a pointer to the addr as a sockaddr_in.
+ */
+#define IA_SIN(ia) (&(((struct in_ifaddr *)(ia))->ia_addr))
+
+int in_ifinit(struct ifnet *dev, struct in_ifaddr *ia, struct sockaddr_in *sin,
+                int scrub);
+
+#endif /* NETINET_IN_VAR_H */
