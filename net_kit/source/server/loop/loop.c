a627a978482b2c83a5b63948ee9dd4ce4841c801
dreid <>
2002-03-18 12:20:00 +0000

    Bring the loopback module in line with changes. - add kernel mode code - add makefile support - add loop_module.h to get us the kernel defines (not sure if this is   going to stay) - change the userland init to new code - adjust where it's installed

--- net_kit/source/server/loop/loop.c
+++ net_kit/source/server/loop/loop.c
@@ -4,6 +4,15 @@
 #include<stdio.h>
 #include <malloc.h>
 
+#ifdef _KERNEL_MODE
+#include <module.h>
+#include "net_server/core_module.h"
+#include "net_device.h"
+#include "loop_module.h"
+
+struct core_module_info *core = NULL;
+#endif
+
 #include "net_module.h"
 #include "sys/socket.h"
 #include "protocols.h"
@@ -31,7 +40,7 @@ int loop_output(ifnet *ifp, struct mbuf *m, struct sockaddr *sa,
 	IFQ_ENQUEUE(ifp->rxq, m);
 }
 
-int loop_input(struct mbuf *buf, int hdrlen)
+int loop_input(struct mbuf *buf)
 {
 	int fproto = IPPROTO_IP; /* XXX - Dirty hack :( */
 
@@ -41,60 +50,108 @@ int loop_input(struct mbuf *buf, int hdrlen)
 		printf("Failed to determine a valid protocol fproto = %d\n", fproto);
 	}
 
+#ifndef _KERNEL_MODE
 	m_freem(buf);
+#else
+	core->m_freem(buf);
+#endif
+
 
 	return 0;
 }
 
-int loop_init(loaded_net_module *ln, int *pt)
+static int loop_dev_stop(ifnet *dev)
 {
-	me = (ifnet*)malloc(sizeof(ifnet));
+	if (!dev || dev->if_type != IFT_LOOP)
+		return EINVAL;
 
-        net_modules = ln;
-        prot_table = pt;
+	dev->flags &= ~IFF_UP;
 
-	me->devid = -1;
-        me->name = "loop";
-        me->unit = 0;
-        me->if_type = IFT_LOOP;
-        me->rxq = NULL;
-        me->rx_thread = -1;
-        me->txq = NULL;
-        me->tx_thread = -1;
-        me->if_addrlist = NULL;
-
-	me->input = &loop_input;
-	me->output = &loop_output;
-	me->ioctl = NULL;
-
-	net_server_add_device(me);
-
-        return 0;
+	kill_thread(dev->rx_thread);
+	kill_thread(dev->tx_thread);
+	
+	return 0;
 }
 
-int loop_dev_init(ifnet *dev)
+static int loop_dev_start(ifnet *dev)
 {
 	if (!dev || dev->if_type != IFT_LOOP)
 		return EINVAL;
 
 	dev->if_mtu = 16384; /* can be as large as we want */
-       	dev->flags |= (IFF_UP|IFF_RUNNING|IFF_MULTICAST|IFF_LOOPBACK);
+	dev->flags |= (IFF_UP|IFF_RUNNING|IFF_MULTICAST|IFF_LOOPBACK);
+	dev->stop = &loop_dev_stop;
+	
+	return 0;
+}
+
+static int loop_init(void)
+{
+	struct ifnet *me = (ifnet*)malloc(sizeof(ifnet));
+	memset(me, 0, sizeof(*me));
+
+	me->devid = -1;
+	me->name = "loop";
+	me->unit = 0;
+	me->if_type = IFT_LOOP;
+	me->rx_thread = -1;
+	me->tx_thread = -1;
+
+	me->start = &loop_dev_start;
+	me->input = &loop_input;
+	me->output = &loop_output;
+
+#ifndef _KERNEL_MODE
+	net_server_add_device(me);
+#else
+	core->add_device(me);
+#endif
 
 	return 0;
 }
 
-net_module net_module_data = {
-	"Loopback Device Driver",
-	NS_LOOP,
-	NET_LAYER1,
-        0,
-        0,
-	0,
 
-	&loop_init,
-	&loop_dev_init,
-	NULL,
-	NULL,
+struct device_info device_info = {
+	"Loopback Device Driver",
+	&loop_init
+};
+
+#ifdef _KERNEL_MODE
+
+static status_t loop_ops(int32 op, ...)
+{
+	switch(op) {
+		case B_MODULE_INIT:
+			if (get_module(CORE_MODULE_PATH, 
+					(module_info **)&core) != B_OK) {
+				dprintf("loop_module: failed to get core ptr\n");
+				return B_ERROR;
+			}
+			break;
+		case B_MODULE_UNINIT:
+			put_module(CORE_MODULE_PATH);
+			break;
+		default:
+			return B_ERROR;
+	}
+	return B_OK;
+}
+
+static struct device_module_info my_module = {
+	{
+		LOOP_MODULE_PATH,
+		B_KEEP_LOADED,
+		loop_ops
+	},
+
+	loop_init
+};
+
+_EXPORT module_info *modules[] = {
+	(module_info*) &my_module,
 	NULL
 };
+
+#endif
+
 
