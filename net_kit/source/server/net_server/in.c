98cae3c89f7dd32bc538b7eb6f8cb4adcb8c3100
dreid <>
2002-03-22 00:36:38 +0000

    Woah! Add in a lot of the configuration stuff (not yet complete) and some more support functions. Also correct some of the problems with byte ordering so we actually get the addresses and masks correctly set. We're going to follow OpenBSD and keep all the addresses in network order as it should reduce the calls to ntohs and friends.
    This works.

--- net_kit/source/server/net_server/in.c
+++ net_kit/source/server/net_server/in.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 #include "netinet/in.h"
-#include "net/if.h"
 #include "sys/socketvar.h"
+#include "net/if.h"
 #include "sys/sockio.h"
 #include "netinet/in_var.h"
 #include "net/route.h"
 
-#define IFA_ROUTE	RTF_UP
+extern struct ifnet **ifnet_addrs;
 
 struct in_ifaddr *get_primary_addr(void)
 {
@@ -32,6 +32,69 @@ void in_socktrim(struct sockaddr_in *ap)
 		}
 }
 
+#define rtinitflags(x) \
+        ((((x)->ia_ifp->flags & (IFF_LOOPBACK | IFF_POINTOPOINT)) != 0) \
+            ? RTF_HOST : 0)
+/*
+ * remove a route to prefix ("connected route" in cisco terminology).
+ * re-installs the route by using another interface address, if there's one
+ * with the same prefix (otherwise we lose the route mistakenly).
+ */
+static int in_scrubprefix(struct in_ifaddr *target)
+{
+        struct in_ifaddr *ia;
+        struct in_addr prefix, mask, p;
+        int error;
+
+        if ((target->ia_flags & IFA_ROUTE) == 0)
+                return 0;
+
+        if (rtinitflags(target))
+                prefix = target->ia_dstaddr.sin_addr;
+        else
+                prefix = target->ia_addr.sin_addr;
+        mask = target->ia_sockmask.sin_addr;
+        prefix.s_addr &= mask.s_addr;
+
+        for (ia = in_ifaddr; ia; ia = ia->ia_next) {
+                /* easy one first */
+                if (mask.s_addr != ia->ia_sockmask.sin_addr.s_addr)
+                        continue;
+
+                if (rtinitflags(ia))
+                        p = ia->ia_dstaddr.sin_addr;
+                else
+                        p = ia->ia_addr.sin_addr;
+                p.s_addr &= ia->ia_sockmask.sin_addr.s_addr;
+                if (prefix.s_addr != p.s_addr)
+                        continue;
+
+                /*
+                 * if we got a matching prefix route, move IFA_ROUTE to him
+                 */
+                if ((ia->ia_flags & IFA_ROUTE) == 0) {
+                        rtinit(&(target->ia_ifa), (int)RTM_DELETE,
+                            rtinitflags(target));
+                        target->ia_flags &= ~IFA_ROUTE;
+
+                        error = rtinit(&ia->ia_ifa, (int)RTM_ADD,
+                            rtinitflags(ia) | RTF_UP);
+                        if (error == 0)
+                                ia->ia_flags |= IFA_ROUTE;
+                        return error;
+                }
+        }
+
+        /*
+         * noone seem to have prefix route.  remove it.
+         */
+        rtinit(&(target->ia_ifa), (int)RTM_DELETE, rtinitflags(target));
+        target->ia_flags &= ~IFA_ROUTE;
+        return 0;
+}
+
+#undef rtinitflags
+
 int in_ifinit(struct ifnet *dev, struct in_ifaddr *ia, struct sockaddr_in *sin,
 		int scrub)
 {
@@ -39,6 +102,8 @@ int in_ifinit(struct ifnet *dev, struct in_ifaddr *ia, struct sockaddr_in *sin,
 	struct sockaddr_in oldsin;
 	int error;
 	int flags = RTF_UP;
+
+printf("in_ifaddr\n");
 
 	oldsin = ia->ia_addr;
 	ia->ia_addr = *sin;
@@ -53,7 +118,7 @@ int in_ifinit(struct ifnet *dev, struct in_ifaddr *ia, struct sockaddr_in *sin,
 
 	if (scrub) {
 		ia->ia_ifa.ifa_addr = (struct sockaddr*)&oldsin;
-		//in_ifscrub(ifp, ia);
+		in_scrubprefix(ia);
 		ia->ia_ifa.ifa_addr = (struct sockaddr*)&ia->ia_addr;
 	}
 
@@ -63,20 +128,21 @@ int in_ifinit(struct ifnet *dev, struct in_ifaddr *ia, struct sockaddr_in *sin,
 		ia->ia_netmask = IN_CLASSB_NET;
 	else
 		ia->ia_netmask = IN_CLASSC_NET;
-
+	
 	if (ia->ia_subnetmask == 0) {
 		ia->ia_subnetmask = ia->ia_netmask;
 		ia->ia_sockmask.sin_addr.s_addr = ia->ia_subnetmask;
 	} else
 		ia->ia_netmask &= ia->ia_subnetmask;
+
 	ia->ia_net = i & ia->ia_netmask;
 	ia->ia_subnet = i & ia->ia_subnetmask;
 	in_socktrim(&ia->ia_sockmask);
 
 	ia->ia_ifa.ifa_metric = dev->if_metric;
 	if (dev->flags & IFF_BROADCAST) {
-		ia->ia_broadaddr.sin_addr.s_addr = htonl(ia->ia_net | ~ia->ia_subnetmask);
-		ia->ia_netbroadcast.s_addr = htonl(ia->ia_net | ~ia->ia_netmask);
+		ia->ia_broadaddr.sin_addr.s_addr = ia->ia_subnet | ~ia->ia_subnetmask;
+		ia->ia_netbroadcast.s_addr = ia->ia_net | ~ia->ia_netmask;
 	} else if (dev->flags & IFF_LOOPBACK) {
 		ia->ia_ifa.ifa_dstaddr = ia->ia_ifa.ifa_addr;
 		flags |= RTF_HOST;
@@ -86,14 +152,21 @@ int in_ifinit(struct ifnet *dev, struct in_ifaddr *ia, struct sockaddr_in *sin,
 		flags |= RTF_HOST;
 	}
 
+printf("in_ifaddr:\n");
+printf("         : ia_net          : %08lx\n", ia->ia_net);
+printf("         : ia_netmask      : %08lx\n", ia->ia_netmask);
+printf("         : ia_subnet       : %08lx\n", ia->ia_subnet);
+printf("         : ia_subnetmask   : %08lx\n", ia->ia_subnetmask);
+printf("         : ia_netbroadcast : %08lx\n", ia->ia_netbroadcast.s_addr);
+printf("         : ia_addr         : %08lx\n", ia->ia_addr.sin_addr.s_addr);
+printf("         : ia_dstaddr      : %08lx\n", ia->ia_dstaddr.sin_addr.s_addr);
+printf("         : ia_sockmask     : %08lx\n", ia->ia_sockmask.sin_addr.s_addr);
+
 	error = rtinit(&(ia->ia_ifa), (int)RTM_ADD, flags);
 	if (error == 0)
 		ia->ia_flags |= IFA_ROUTE;
 
 	/* XXX - Multicast address list */
-
-	if (!in_ifaddr)
-		in_ifaddr = ia;
 
 	return error;
 }
@@ -106,18 +179,35 @@ int in_control(struct socket *so, int cmd, caddr_t data, struct ifnet *ifp)
 	struct in_ifaddr *oia;
 	struct in_aliasreq *ifra = (struct in_aliasreq*)data;
 	struct sockaddr_in oldaddr;
-	int error, hostIsNew, maskIsNew;
+	int error = 0, hostIsNew, maskIsNew;
 	long i;
 
 	if (ifp) /* we need to find the in_ifaddr */
-		for (ia = in_ifaddr;ia; ia = ia->ia_next)
+		for (ia = in_ifaddr;ia; ia = ia->ia_next) {
+			printf("ia = %p, ia->ifp = %p\n", ia, ia->ia_ifp);
 			if (ia->ia_ifp == ifp)
 				break;
-	
+		}
+		
 	switch (cmd) {
+		case SIOCAIFADDR:
+			/* add an address */
+		case SIOCDIFADDR:
+			/* delete an address */
+			if (ifra->ifra_addr.sin_family == AF_INET)
+				for (oia = ia; ia; ia = ia->ia_next) {
+					if (ia->ia_ifp == ifp &&
+					    ia->ia_addr.sin_addr.s_addr == ifra->ifra_addr.sin_addr.s_addr)
+						break;
+				}
+			if (cmd == SIOCDIFADDR && ia == NULL)
+				return EADDRNOTAVAIL;
 		case SIOCSIFADDR:
+			/* set an address */
 		case SIOCSIFNETMASK:
+			/* set a net mask */
 		case SIOCSIFDSTADDR:
+			/* set the destination address of a point to point link */
 			if (!ifp) {
 				printf("No interface pointer!\n");
 				return EINVAL;
@@ -154,16 +244,57 @@ int in_control(struct socket *so, int cmd, caddr_t data, struct ifnet *ifp)
 				ia->ia_ifp = ifp;
 			}
 			break;
+		case SIOCSIFBRDADDR:
+		case SIOCGIFADDR:
+		case SIOCGIFNETMASK:
+		case SIOCGIFDSTADDR:
+		case SIOCGIFBRDADDR:
+			if (ia == NULL)
+				return EADDRNOTAVAIL;
+			break;
+				
 	}
 	
 	switch(cmd) {
+		case SIOCGIFADDR:
+			/* get interface address */
+			printf("SIOCGIFADDR\n");
+			*((struct sockaddr_in*) &ifr->ifr_addr) = ia->ia_addr;
+			break;
+		case SIOCGIFDSTADDR:
+			/* get interface point to point destination address */
+			if ((ifp->flags & IFF_POINTOPOINT) == 0)
+				/* we're not a point to point interface */
+				return EINVAL;
+			*((struct sockaddr_in*) &ifr->ifr_dstaddr) = ia->ia_dstaddr;
+			break;
+		case SIOCGIFBRDADDR:
+			/* get interface broadcast address */
+			if ((ifp->flags & IFF_BROADCAST) == 0)
+				/* we're not a broadcast capable interface */
+				return EINVAL;
+			*((struct sockaddr_in*) &ifr->ifr_dstaddr) = ia->ia_broadaddr;
+			break;
+		case SIOCGIFNETMASK:
+			/* get interface netmask */
+			printf("SIOCGIFNETMASK\n");
+			*((struct sockaddr_in*) &ifr->ifr_addr) = ia->ia_sockmask;
+			break;
 		case SIOCSIFADDR:
+			printf("SIOCSIFADDR\n");	
 			return in_ifinit(ifp, ia, (struct sockaddr_in*)&ifr->ifr_addr, 1);
 		case SIOCSIFNETMASK:
+			printf("SIOCSIFNETMASK\n");
+			/* set the netmask for the interface... */
+			/* set i to the network netmask (network host order) */
 			i = ifra->ifra_addr.sin_addr.s_addr;
-			ia->ia_subnetmask = ntohl(ia->ia_sockmask.sin_addr.s_addr = i);
+			/* set the sockmask to the netmask */
+			ia->ia_sockmask.sin_addr.s_addr = i;
+			/* set the host byte order netmask into ia_subnetmask */
+			ia->ia_subnetmask = ntohl(i);
 			break;
 		case SIOCSIFDSTADDR:
+			printf("SIOCSIFDSTADDR\n");
 			if ((ifp->flags & IFF_POINTOPOINT) == 0)
 				return EINVAL;
 			oldaddr = ia->ia_dstaddr;
@@ -183,12 +314,117 @@ int in_control(struct socket *so, int cmd, caddr_t data, struct ifnet *ifp)
 				ia->ia_ifa.ifa_dstaddr = (struct sockaddr*)&ia->ia_dstaddr;
 				rtinit(&(ia->ia_ifa), RTM_ADD, RTF_HOST|RTF_UP);
 			}
+			break;
+
+		case SIOCSIFBRDADDR:
+			printf("SIOCSIFBRDADDR\n");
+			/* set the broadcast address if interface supports it */
+			if ((ifp->flags & IFF_BROADCAST) == 0)
+				/* we don't support broadcast on that interface */
+				return EINVAL;
+			ia->ia_broadaddr = *(struct sockaddr_in*) &ifr->ifr_broadaddr;
+			break;
+		case SIOCAIFADDR:
+			printf("SIOCAIFADDR\n");
+			maskIsNew = 0;
+			hostIsNew = 1;
+			error = 0;
+			if (ia->ia_addr.sin_family == AF_INET) {
+				if (ifra->ifra_addr.sin_len == 0) {
+					ifra->ifra_addr = ia->ia_addr;
+					hostIsNew = 0;
+				} else if (ifra->ifra_addr.sin_addr.s_addr == 
+				           ia->ia_addr.sin_addr.s_addr)
+					hostIsNew = 0;
+			}
+			if (ifra->ifra_mask.sin_len) {
+				in_scrubprefix(ia);
+				ia->ia_sockmask = ifra->ifra_mask;
+				ia->ia_subnetmask = ia->ia_sockmask.sin_addr.s_addr;
+				maskIsNew = 1;
+			}
+			if ((ifp->flags & IFF_POINTOPOINT) &&
+			    (ifra->ifra_dstaddr.sin_family == AF_INET)) {
+				in_scrubprefix(ia);
+				ia->ia_dstaddr = ifra->ifra_dstaddr;
+				maskIsNew = 1;
+			}
+			if (ifra->ifra_addr.sin_family == AF_INET &&
+			    (hostIsNew || maskIsNew))
+				error = in_ifinit(ifp, ia, &ifra->ifra_addr, 0);
+			if ((ifp->flags & IFF_BROADCAST) &&
+			    (ifra->ifra_broadaddr.sin_family == AF_INET))
+				ia->ia_broadaddr = ifra->ifra_broadaddr;
+			return error;
 		default:
 			/* if we don't have enough to do the default, return */
 			if (ifp == NULL || ifp->ioctl == NULL)
-				return EINVAL; /* XXX - should be ENOTSUPP */
+				return EINVAL; /* XXX - should be EOPNOTSUPP */
 			/* send to the card and let it process it */
 			return ifp->ioctl(ifp, cmd, data);
 	}
 	return 0;
 }
+
+
+
+/*
+ * Return 1 if the address might be a local broadcast address.
+ */
+int in_broadcast(struct in_addr in, struct ifnet *ifp)
+{
+	struct ifnet *ifn, *if_first, *if_target;
+	struct ifaddr *ifa;
+
+	if (in.s_addr == INADDR_BROADCAST ||
+	    in.s_addr == INADDR_ANY)
+		return 1;
+	if (ifp && ((ifp->flags & IFF_BROADCAST) == 0))
+		return 0;
+
+	if (ifp == NULL) {
+		if_first = *ifnet_addrs;
+		if_target = 0;
+	} else {
+		if_first = ifp;
+		if_target = ifp->next;
+	}
+
+#define ia (ifatoia(ifa))
+	/*
+	 * Look through the list of addresses for a match
+	 * with a broadcast address.
+	 * If ifp is NULL, check against all the interfaces.
+	 */
+	for (ifn = if_first; ifn != if_target; ifn = ifn->next) {
+		for (ifa = ifn->if_addrlist; ifa; ifa = ifa->ifa_next) {
+			if (!ifp) {
+				if (ifa->ifa_addr->sa_family == AF_INET &&
+				    ((ia->ia_subnetmask != 0xffffffff &&
+				    (((ifn->flags & IFF_BROADCAST) &&
+				    in.s_addr == ia->ia_broadaddr.sin_addr.s_addr) ||
+				    in.s_addr == ia->ia_subnet)) ||
+				    /*
+				     * Check for old-style (host 0) broadcast.
+				     */
+				    (in.s_addr == ia->ia_netbroadcast.s_addr ||
+				    in.s_addr == ia->ia_net)))
+					return 1;
+				else
+					if (ifa->ifa_addr->sa_family == AF_INET &&
+					    (((ifn->flags & IFF_BROADCAST) &&
+					    in.s_addr == ia->ia_broadaddr.sin_addr.s_addr) ||
+					    in.s_addr == ia->ia_netbroadcast.s_addr ||
+					    /*
+					     * Check for old-style (host 0) broadcast.
+					     */
+					    in.s_addr == ia->ia_subnet ||
+					    in.s_addr == ia->ia_net))
+						return 1;
+			}
+		}
+	}
+	return (0);
+#undef ia
+}
+
