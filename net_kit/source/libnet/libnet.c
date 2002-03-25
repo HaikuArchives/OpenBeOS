c11ade723255da5efa75cdf6474b9e70a6c1d874
dreid <>
2002-03-25 11:33:48 +0000

    Bring the libnet code up to date with all the things we can now do!
    This also adds the structures and the defines needed by the socket
    driver.

--- net_kit/source/libnet/libnet.c
+++ net_kit/source/libnet/libnet.c
@@ -5,7 +5,11 @@
  */
 #include <fcntl.h>
 #include <unistd.h>
+#include <kernel/OS.h>
+#include <iovec.h>
+#include <stdio.h>
 
+#include "sys/socket.h"
 #include "net_structures.h"
 
 int socket(int domain, int type, int protocol)
@@ -33,3 +37,80 @@ int closesocket(int sock)
 	return close(sock);
 }
 
+int bind(int sock, const struct sockaddr *sa, int len)
+{
+	struct bind_args ba;
+	
+	ba.data = (caddr_t)sa;
+	ba.dlen = len;
+	
+	return ioctl(sock, NET_SOCKET_BIND, &ba, sizeof(ba));
+}
+
+int recvfrom(int sock, caddr_t buffer, size_t buflen, int flags,
+             struct sockaddr *addr, size_t addrlen)
+{
+	struct msghdr mh;
+	struct iovec iov;
+
+	/* XXX - would this be better done as scatter gather? */	
+	mh.msg_name = (caddr_t)addr;
+	mh.msg_namelen = addrlen;
+	mh.msg_flags = flags;
+	mh.msg_control = NULL;
+	mh.msg_controllen = 0;
+	iov.iov_base = buffer;
+	iov.iov_len = buflen;
+	mh.msg_iov = &iov;
+	mh.msg_iovlen = 1;
+
+	return ioctl(sock, NET_SOCKET_RECVFROM, &mh, sizeof(mh));
+}
+
+int sendto(int sock, caddr_t buffer, size_t buflen, int flags,
+           const struct sockaddr *addr, size_t addrlen)
+{
+	struct msghdr mh;
+	struct iovec iov;
+
+	/* XXX - would this be better done as scatter gather? */	
+	mh.msg_name = (caddr_t)addr;
+	mh.msg_namelen = addrlen;
+	mh.msg_flags = flags;
+	mh.msg_control = NULL;
+	mh.msg_controllen = 0;
+	iov.iov_base = buffer;
+	iov.iov_len = buflen;
+	mh.msg_iov = &iov;
+	mh.msg_iovlen = 1;
+
+	return ioctl(sock, NET_SOCKET_SENDTO, &mh, sizeof(mh));
+}
+
+int net_select(int nbits, struct fd_set *rbits, 
+                      struct fd_set *wbits, 
+                      struct fd_set *ebits, 
+                      struct timeval *timeout)
+{
+	int tmpfd = open("/dev/net/socket", O_RDWR);
+	struct select_args sa;
+	int error;
+	int i;
+	
+	sa.mfd = nbits;
+	sa.rbits = rbits;
+	sa.wbits = wbits;
+	sa.ebits = ebits;
+	sa.tv = timeout;
+	
+	for (i=2; i <10;i++) {
+		if (rbits)
+			if (FD_ISSET(i, rbits))
+				printf("socket %d set in read bits...\n", i);
+	}
+	
+	error = ioctl(tmpfd, NET_SOCKET_SELECT, &sa, sizeof(sa));
+printf("error = %d\n", error);
+	close(tmpfd);
+	return (error);
+}
--- net_kit/source/libnet/net_structures.h
+++ net_kit/source/libnet/net_structures.h
@@ -6,6 +6,8 @@
 #define NET_STRCUTURES_H
 
 #include <Drivers.h>
+#include "sys/select.h"
+#include <sys/time.h>
 
 struct socket_args {
 	int dom;
@@ -13,9 +15,37 @@ struct socket_args {
 	int prot;
 };
 
+struct bind_args {
+	caddr_t data;
+	int dlen;
+};
+
+struct listen_args {
+	int backlog;
+};
+
+struct connect_args {
+	caddr_t name;
+	int namelen;
+};
+
+struct select_args {
+	int mfd;
+	struct fd_set *rbits;
+	struct fd_set *wbits;
+	struct fd_set *ebits;
+	struct timeval *tv;
+};
+	
 /* To simplyify the addition of ones! */
 enum {
-	NET_SOCKET_CREATE = B_DEVICE_OP_CODES_END + 0x100
+	NET_SOCKET_CREATE = B_DEVICE_OP_CODES_END + 0x100,
+	NET_SOCKET_BIND,
+	NET_SOCKET_RECVFROM,
+	NET_SOCKET_SENDTO,
+	NET_SOCKET_LISTEN,
+	NET_SOCKET_CONNECT,
+	NET_SOCKET_SELECT
 };
 
 
