ecbbab879c2ff1ef925e8af0b004dbbe543c0b40
tylerdauwalder <>
2002-03-30 06:40:31 +0000

    Added op=(), op==(), op!=(), and all constructors except the BDirectory version

--- storage_kit/source/lib/Path.cpp
+++ storage_kit/source/lib/Path.cpp
@@ -5,8 +5,9 @@
 //  File Name: Path.cpp
 //  Description: An absolute pathname wrapper class
 //---------------------------------------------------------------------
-#include "Path.h"
-#include "Error.h"
+#include <Path.h>
+
+#include <Entry.h>
 
 #ifdef USE_OPENBEOS_NAMESPACE
 using namespace OpenBeOS;
@@ -88,52 +89,104 @@ bool BPath::MustNormalize(const char *path) {
 	
 }
 
-BPath::BPath() : fName(NULL) {
+BPath::BPath() : fCStatus(B_NO_INIT), fName(NULL) {
 }
 	
-BPath::BPath(const char *dir, const char *leaf = NULL, bool normalize = false) {
+BPath::BPath(const char *dir, const char *leaf = NULL, bool normalize = false) :
+fCStatus(B_NO_INIT), fName(NULL)
+{
+	SetTo(dir, leaf, normalize);
 }
 	
-BPath::BPath(const BDirectory *dir, const char *leaf, bool normalize = false) {
+BPath::BPath(const BDirectory *dir, const char *leaf, bool normalize = false) :
+fCStatus(B_NO_INIT), fName(NULL)
+{
+	SetTo(dir, leaf, normalize);
 }
 
-BPath::BPath(const BPath &path) {
+BPath::BPath(const BPath &path) : fCStatus(B_NO_INIT), fName(NULL) {
+	SetTo(path.Path());
 }
 
-BPath::BPath(const BEntry *entry) {
+BPath::BPath(const BEntry *entry) : fCStatus(B_NO_INIT), fName(NULL) {
+	SetTo(entry);
 }
 
-BPath::BPath(const entry_ref *ref) {
+BPath::BPath(const entry_ref *ref) : fCStatus(B_NO_INIT), fName(NULL) {
 }
 
 BPath::~BPath() {
+	Unset();
 }
 
-
-
 status_t BPath::InitCheck() const {
-	return B_NO_INIT;
+	return fCStatus;
 }
 
 status_t BPath::SetTo(const char *path, const char *leaf = NULL, bool normalize = false) {
+	Unset();
+
+	if (path == NULL)
+		return B_BAD_VALUE;
+	
+	if (normalize) {
+		// I'll deal with this later :-)
+		return B_FILE_ERROR;
+	} else {
+		if (fName != NULL)
+			delete [] fName;
+			
+		fName = new char[strlen(path)+1];
+		if (fName == NULL)
+			return B_NO_MEMORY;
+		strcpy(fName, path);
+	}
+	
+	fCStatus = B_OK;
+	return B_OK;
 }
 	
 status_t BPath::SetTo(const BDirectory *dir, const char *path, bool normalize = false) {
+	//! @todo Implement this once we have a BDirectory implementation
+	Unset();
+	return B_FILE_ERROR;
 }
 	
 status_t BPath::SetTo(const BEntry *entry) {
+	Unset();
+	if (entry == NULL) 
+		return B_BAD_VALUE;
+
+	// Convert the Entry to an entry ref
+	entry_ref ref;
+	status_t result = entry->GetRef(&ref);
+
+	return (result == B_OK) ? SetTo(&ref) : result ;
 }
 	
 status_t BPath::SetTo(const entry_ref *ref) {
+	Unset();
+	if (ref == NULL) 
+		return B_BAD_VALUE;
+		
+	char path[B_PATH_NAME_LENGTH+1];
+	status_t result = StorageKit::entry_ref_to_path(ref, path, B_PATH_NAME_LENGTH+1);
+	return (result == B_OK) ? SetTo(path) : result ;
 }
 
 status_t BPath::Append(const char *path, bool normalize = false) {
 }
 	
 void BPath::Unset() {
+	if (fCStatus == B_OK) {
+		delete [] fName;
+		fName = NULL;
+	}
+	fCStatus = B_NO_INIT;
 }
 	
 const char *BPath::Path() const {
+	return fName;
 }
 	
 const char *BPath::Leaf() const {
@@ -143,22 +196,37 @@ status_t BPath::GetParent(BPath *) const {
 }
 	
 bool BPath::operator==(const BPath &item) const {
+	return *this == item.Path();
 }
 
 bool BPath::operator==(const char *path) const {
+	printf("==() fName == '%s'\n", fName);
+	printf("==() path  == '%s'\n", path);
+	return (strcmp(fName, path) == 0);
 }
 
 bool BPath::operator!=(const BPath &item) const {
+	return !(*this == item.Path());
 }
 
 bool BPath::operator!=(const char *path) const {
+	return !(*this == path);
 }
 	
 
 BPath& BPath::operator=(const BPath &item) {
+	if (this != &item)
+		*this = item.fName;
+	return *this;
 }
 
 BPath& BPath::operator=(const char *path) {
+	if (path == NULL) {
+		Unset();
+	} else {
+		SetTo(path);
+	}
+	return *this;
 }
 
 	
