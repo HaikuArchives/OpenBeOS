9d6e70cce506e794e6c59bd4985a9ad6924b1c38
dreid <>
2002-04-02 12:59:06 +0000

    The header file for the new module definition.

---
+++ net_kit/source/server/ipv4/ipv4_module.h
@@ -0,0 +1,26 @@
+/* ipv4_module.h
+ * definitions for ipv4 protocol
+ */
+ 
+#ifndef IPV4_MODULE_H
+#define IPV4_MODULE_H
+
+#ifdef _KERNEL_MODE
+
+#include <KernelExport.h>
+#include <module.h>
+
+struct ipv4_module_info {
+	module_info info;
+	int (*ip_output)(struct mbuf *, 
+	                 struct mbuf *, 
+	                 struct route *,
+	                 int, void *);
+	uint16 (*ip_id)(void);
+};
+
+#define IPV4_MODULE_PATH	"network/protocol/ipv4"
+
+#endif /* _KERNEL_MODE */
+
+#endif /* IPV4_MODULE_H */
