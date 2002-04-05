7ce85539b037278175e7a62b821b78c4f174561b
tylerdauwalder <>
2002-04-05 06:05:05 +0000

    initial checkin; forgot the reserved virtuals the first time around, so here they are

---
+++ storage_kit/source/lib/EntryList.cpp
@@ -0,0 +1,13 @@
+#include <EntryList.h>
+
+BEntryList::BEntryList() {}
+BEntryList::~BEntryList() {}
+void BEntryList::_ReservedEntryList1() {}
+void BEntryList::_ReservedEntryList2() {}
+void BEntryList::_ReservedEntryList3() {}
+void BEntryList::_ReservedEntryList4() {}
+void BEntryList::_ReservedEntryList5() {}
+void BEntryList::_ReservedEntryList6() {}
+void BEntryList::_ReservedEntryList7() {}
+void BEntryList::_ReservedEntryList8() {}
+
