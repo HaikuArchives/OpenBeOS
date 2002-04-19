92e6cd94ac6baf8e7bc63d2a9619ce2a0ade8f4a
tylerdauwalder <>
2002-04-19 04:37:43 +0000

    Initial checkin. To be used for private declarations that must be used in public headers.

---
+++ storage_kit/source/lib/StorageDefs.Private.h
@@ -0,0 +1,16 @@
+//----------------------------------------------------------------------
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//
+//  File Name: StorageDefs.Private.h
+//	Description: Private Storage Kit declarations that needed in
+//				 public headers.
+//---------------------------------------------------------------------
+#ifndef __sk_def_storage_private_h__
+#define __sk_def_storage_private_h__
+
+namespace StorageKit {
+	typedef int FileDescriptor;
+};
+
+#endif // __sk_def_storage_private_h__
