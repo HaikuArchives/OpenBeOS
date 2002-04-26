990b1b375d3e1cf44c991e7cfbc6da95b9cb723d
bonefish <>
2002-04-26 09:20:51 +0000

    Added a rudimentary BString implementation for BNode::Read/WriteAttrString(). To be replaced when the IK team version is ready.

---
+++ storage_kit/source/lib/String.cpp
@@ -0,0 +1,114 @@
+// Very, very basic BString implementation -- neither binary compatible nor
+// complete. Just here to allow for a complete BNode implementation.
+// To be replaced by the OpenBeOS version to be provided by the IK Team.
+
+#include <stdio.h>
+#include <string.h>
+
+#include "String.h"
+
+// constructor
+BString::BString()
+	   : fData(NULL)
+{
+	_Init();
+}
+
+// constructor
+BString::BString(const char *str)
+	   : fData(NULL)
+{
+	_Init(str);
+}
+
+// copy constructor
+BString::BString(const BString &str)
+	   : fData(NULL)
+{
+	*this = str;
+}
+
+// destructor
+BString::~BString()
+{
+	delete[] fData;
+}
+
+// String
+const char *
+BString::String() const
+{
+	return fData;
+}
+
+// Length
+int32
+BString::Length() const
+{
+	return strlen(fData);
+}
+
+// LockBuffer
+char *
+BString::LockBuffer(int32 maxLength)
+{
+	int32 len = Length();
+	if (maxLength > len)
+		_Init(fData, maxLength);
+	return fData;
+}
+
+// UnlockBuffer
+BString &
+BString::UnlockBuffer(int32 length)
+{
+	if (length < 0)
+		length = 0;
+	fData[length] = 0;	// null terminate
+}
+
+// =
+BString &
+BString::operator=(const BString& str)
+{
+	_Init(str.String());
+	return *this;
+}
+
+// ==
+bool
+BString::operator==(const BString& str)
+{
+	return (strcmp(fData, str.fData) == 0);
+}
+
+// !=
+bool
+BString::operator!=(const BString& str)
+{
+	return !(*this == str);
+}
+
+// _Init
+//
+// len -- length of the maximal string that shall fit into the new allocation,
+//		  if < 0, strlen(str) is used instead
+void
+BString::_Init(const char *str, int32 len)
+{
+//printf("BString::_Init(%p, %ld)\n", str, len);
+	// copy the supplied string
+	char *newData = NULL;
+	if (!str)
+		str = "";
+	if (len < 0)
+		len = strlen(str);
+	newData = new char[len + 1];
+	strncpy(newData, str, len);
+	newData[len] = 0;
+	// delete the old data and set the new
+	delete[] fData;
+	fData = newData;
+//printf("BString::_Init() done\n");
+}
+
