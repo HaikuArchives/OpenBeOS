fb370a2d0a0b2095e02c3394623fac2246045fb1
bonefish <>
2002-04-28 17:35:49 +0000

    * Almost complete reimplementation of all tests. * Now derives from StatableTest and performs these tests.

--- storage_kit/test/EntryTest.h
+++ storage_kit/test/EntryTest.h
@@ -1,9 +1,10 @@
+// EntryTest.h
+
 #ifndef __sk_entry_test_h__
 #define __sk_entry_test_h__
 
-#include <cppunit/TestCase.h>
-#include <cppunit/TestCaller.h>
-#include <cppunit/TestSuite.h>
+#include "StatableTest.h"
+
 #include "Test.StorageKit.h"
 
 #include <Entry.h>
@@ -11,35 +12,18 @@
 #include <Directory.h>
 #include <Path.h>
 
-#include <sys/utsname.h> // Not needed
-#include <sys/statvfs.h> // Not needed
-
-//#include <stdio.h>
-
-//#include <sys/stat.h>	// For struct stat
-//#include <fs_attr.h>	// For struct attr_info
-
-//#include <kernel_interface.h>
-
 #include "TestUtils.h"
 
-class EntryTest : public StorageKit::TestCase
+struct TestEntry;
+
+class EntryTest : public StatableTest
 {
 public:
 	static CppUnit::Test* Suite();
 
-	// Filenames
-	static const char mainFile[] = "/boot/beos/apps/Clock";
-	static const char link[] = "EntryTest.Link";
-	static const char relLink[] = "EntryTest.RelLink";
-	static const char fourLink[] = "EntryTest.4Link";
-	static const char tripleLinkLeaf[] = "EntryTest.TripleLink";
-	char tripleLink[B_PATH_NAME_LENGTH+1];
-	static const char sparkleMotion[] = "/boot/I_am_beggining_to_doubt_your_dedication_to_Sparkle_Motion";
-	static const char someFile[] = "/boot/SomeFileNameThatWeReallyReallyLikeALot";
-
-	// Constructor
-	EntryTest();
+	virtual void CreateROStatables(TestStatables& testEntries);
+	virtual void CreateRWStatables(TestStatables& testEntries);
+	virtual void CreateUninitializedStatables(TestStatables& testEntries);
 
 	// This function called before *each* test added in Suite()
 	void setUp();
@@ -50,31 +34,84 @@ public:
 	//-------------------------------------------------------------------------
 	// Test functions
 	//-------------------------------------------------------------------------
-	void TraversalTest();
-	void CopyConstructorTest();
-	void EqualityTest();
-	void AssignmentTest();	
-	void ConversionTest();
-	void MiscTest();
+	void InitTest1Paths(TestEntry &testEntry, status_t error,
+						bool traverse = false);
+	void InitTest1Refs(TestEntry &testEntry, status_t error,
+					   bool traverse = false);
+	void InitTest1DirPaths(TestEntry &testEntry, status_t error,
+						   bool traverse = false);
+	void InitTest1();
+
+	void InitTest2Paths(TestEntry &testEntry, status_t error,
+						bool traverse = false);
+	void InitTest2Refs(TestEntry &testEntry, status_t error,
+					   bool traverse = false);
+	void InitTest2DirPaths(TestEntry &testEntry, status_t error,
+						   bool traverse = false);
+	void InitTest2();
+
 	void ExistenceTest();
+	void SpecialGetCasesTest();
+
+	void RenameTestEntry(TestEntry *testEntry, TestEntry *newTestEntry,
+						 string newName, bool existing, bool clobber,
+						 status_t error, uint32 kind);
+	void RenameTestEntry(TestEntry *testEntry, TestEntry *newTestEntry,
+						 bool existing, bool clobber,
+						 status_t error, uint32 kind);
+	void RenameTestFile(TestEntry *testEntry, TestEntry *newTestEntry,
+						bool existing, bool clobber, status_t error);
+	void RenameTestDir(TestEntry *testEntry, TestEntry *newTestEntry,
+					   bool existing, bool clobber, status_t error);
+	void RenameTestLink(TestEntry *testEntry, TestEntry *newTestEntry,
+						bool existing, bool clobber, status_t error);
 	void RenameTest();
-	void StatTest();
+
+	void MoveToTestEntry(TestEntry *testEntry, TestEntry *testDir,
+						 string newName, bool existing, bool clobber,
+						 status_t error, uint32 kind);
+	void MoveToTestEntry(TestEntry *testEntry, TestEntry *testDir,
+						 TestEntry *newTestEntry, bool existing, bool clobber,
+						 status_t error, uint32 kind);
+	void MoveToTestFile(TestEntry *testEntry, TestEntry *testDir,
+						TestEntry *newTestEntry, bool existing, bool clobber,
+						status_t error);
+	void MoveToTestDir(TestEntry *testEntry, TestEntry *testDir,
+					   TestEntry *newTestEntry, bool existing, bool clobber,
+					   status_t error);
+	void MoveToTestLink(TestEntry *testEntry, TestEntry *testDir,
+						TestEntry *newTestEntry, bool existing, bool clobber,
+						status_t error);
+	void MoveToTest();
+
+	void RemoveTest();
+	void ComparisonTest();
+	void AssignmentTest();	
+	void MiscTest();
 
 	//-------------------------------------------------------------------------
 	// Helper functions
 	//-------------------------------------------------------------------------	
-
 	// Creates a symbolic link named link that points to target
-	bool CreateLink(const char *link, const char *target);
+	void CreateLink(const char *link, const char *target);
 	// Creates the given file
-	bool CreateFile(const char *file);
-	// Removes the given file if it exsists
-	bool RemoveFile(const char *file);
-
+	void CreateFile(const char *file);
+	// Creates the given file
+	void CreateDir(const char *dir);
+	// Removes the given file if it exists
+	void RemoveFile(const char *file);
+	// Checks, whether a file exists.
+	bool PingFile(const char *path, BEntry *entry = NULL);
+	// Checks, whether a dir exists.
+	bool PingDir(const char *path, BEntry *entry = NULL);
+	// Checks, whether a symlink exists.
+	bool PingLink(const char *path, const char *target = NULL,
+				  BEntry *entry = NULL);
+/*
 	// n1 and n2 should both be uninitialized. y1a and y1b should be initialized
 	// to the same entry, y2 should be initialized to a different entry
 	void EqualityTest(BEntry &n1, BEntry &n2, BEntry &y1a, BEntry &y1b, BEntry &y2);
-
+*/
 };
 
 
