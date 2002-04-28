726686ef74d3cada3253a7aae7c680342853582a
mwilber <>
2002-04-28 14:04:39 +0000

    I changed this file so that it meets the OpenTracker style guidelines. I changed the variable names, spacing, indenting, comments and things of that nature.

--- openbeos/headers/obos/public/translation/TranslationUtils.h
+++ openbeos/headers/obos/public/translation/TranslationUtils.h
@@ -16,113 +16,94 @@
 #include <TranslationDefs.h>
 #include <SupportDefs.h>
 
-
 class BBitmap;
 class BTranslatorRoster;
 class BPositionIO;
 class BMenu;
 
-
-/* This class is a little different from many other classes. */
-/* You don't create an instance of it; you just call its various */
-/* static member functions for utility-like operations. */
-
-class BTranslationUtils
-{
-		BTranslationUtils();
-		~BTranslationUtils();
-		BTranslationUtils(
-				const BTranslationUtils &);
-		BTranslationUtils & operator=(
-				const BTranslationUtils &);
+// This class is a little different from many other classes.
+// You don't create an instance of it; you just call its various
+// static member functions for utility-like operations.
+class BTranslationUtils {
+	BTranslationUtils();
+	~BTranslationUtils();
+	BTranslationUtils(const BTranslationUtils &);
+	BTranslationUtils & operator=(const BTranslationUtils &);
 
 public:
 
-/***	BITMAP getters - allocate the BBitmap; you call delete on it!	***/
+// BITMAP getters - allocate the BBitmap; you call delete on it!
 
-		/*	Get bitmap - first try as file name, then as B_TRANSLATOR_BITMAP */
-		/*	resource type from app file -- can be of any kind for which a */
-		/*	translator is installed (TGA, etc) */
-static	BBitmap * GetBitmap(
-				const char * name,
-				BTranslatorRoster * use = NULL);
-		/*	Get bitmap - from app resource file only */
-static	BBitmap * GetBitmap(
-				uint32 type,
-				int32 id,
-				BTranslatorRoster * use = NULL);
-static	BBitmap * GetBitmap(
-				uint32 type,
-				const char * name,
-				BTranslatorRoster * use = NULL);
-		/*	Get bitmap - from file only	*/
-static	BBitmap * GetBitmapFile(
-				const char * name,
-				BTranslatorRoster * use = NULL);
-static	BBitmap * GetBitmap(
-				const entry_ref * ref,
-				BTranslatorRoster * use = NULL);
-		/*	Get bitmap - from open file (or BMemoryIO) */
-static	BBitmap * GetBitmap(
-				BPositionIO * stream,	/*	not NULL	*/
-				BTranslatorRoster * use = NULL);
+static BBitmap *GetBitmap(const char *kName, BTranslatorRoster *roster = NULL);
+	// Get bitmap - first try as file name, then as B_TRANSLATOR_BITMAP
+	// resource type from app file -- can be of any kind for which a
+	// translator is installed (TGA, etc)
+	
+static BBitmap *GetBitmap(uint32 type, int32 id, BTranslatorRoster *roster = NULL);
+	// Get bitmap - from app resource by id
+	
+static BBitmap *GetBitmap(uint32 type, const char *kName,
+	BTranslatorRoster *roster = NULL);
+	// Get Bitmap - from app resource by name
+	
+static BBitmap *GetBitmapFile(const char *kName, BTranslatorRoster *roster = NULL);
+	// Get bitmap - from file only
 
-		/* For styled text, we can translate from a stream directly into a BTextView */
-static	status_t GetStyledText(
-				BPositionIO * fromStream,
-				BTextView * intoView,	/*	not NULL	*/
-				BTranslatorRoster * use = NULL);
-		/* Saving is slightly different -- given the BTextView, a B_STYLED_TEXT_FORMAT */
-		/* stream is written to intoStream. You can then call Translate() yourself to */
-		/* translate that stream into something else if you want, if it is a temp file */
-		/* or a BMallocIO. It's also OK to write the B_STYLED_TEXT_FORMAT to a file, */
-		/* although the StyledEdit format (TEXT file + style attributes) is preferred. */
-static	status_t PutStyledText(
-				BTextView * fromView,
-				BPositionIO * intoStream,
-				BTranslatorRoster * use = NULL);
-		/* This convenience function is only marginally part of the Translation Kit, */
-		/* but what the hey :-) */
-static	status_t WriteStyledEditFile(
-				BTextView * fromView,
-				BFile * intoFile);
+static BBitmap *GetBitmap(const entry_ref *kRef, BTranslatorRoster *roster = NULL);
+	// Get bitmap - from open file (or BMemoryIO)
+		
+static BBitmap *GetBitmap(BPositionIO *stream, BTranslatorRoster *roster = NULL);
+	// Get bitmap - from open file or IO type object
 
-		/* Each translator can have default settings, set by the "translations" */
-		/* control panel. You can read these settings to pass on to a translator */
-		/* using one of these functions. */
-static	BMessage * GetDefaultSettings(
-				translator_id for_translator,
-				BTranslatorRoster * use = NULL);
-static	BMessage * GetDefaultSettings(
-				const char * translator_name,
-				int32 translator_version);
+static status_t GetStyledText(BPositionIO *fromStream, BTextView *intoView,
+	BTranslatorRoster *roster = NULL);
+	// For styled text, we can translate from a stream
+	// directly into a BTextView
 
-		/* Envious of that "Save As" menu in ShowImage? Well, you can have your own! */
-		/* AddTranslationItems will add menu items for all translations from the */
-		/* basic format you specify (B_TRANSLATOR_BITMAP, B_TRANSLATOR_TEXT etc). */
-		/* The translator ID and format constant chosen will be added to the message */
-		/* that is sent to you when the menu item is selected. */
-		enum {
-			B_TRANSLATION_MENU = 'BTMN'
-		};
-static	status_t AddTranslationItems(
-				BMenu * intoMenu,
-				uint32 from_type,
-				const BMessage * model = NULL,	/* default B_TRANSLATION_MENU */
-				const char * translator_id_name = NULL, /* default "be:translator" */
-				const char * translator_type_name = NULL, /* default "be:type" */
-				BTranslatorRoster * use = NULL);
+static status_t PutStyledText(BTextView *fromView, BPositionIO *intoStream,
+	BTranslatorRoster *roster = NULL);
+	// Saving is slightly different -- given the BTextView, a B_STYLED_TEXT_FORMAT
+	// stream is written to intoStream. You can then call Translate() yourself to
+	// translate that stream into something else if you want, if it is a temp file
+	// or a BMallocIO. It's also OK to write the B_STYLED_TEXT_FORMAT to a file,
+	// although the StyledEdit format (TEXT file + style attributes) is preferred.
+	// This convenience function is only marginally part of the Translation Kit,
+	// but what the hey :-)
+	
+static status_t WriteStyledEditFile(BTextView *fromView, BFile *intoFile);
+	// Writes the styled text data from fromView to intoFile
+
+static BMessage *GetDefaultSettings(translator_id forTranslator,
+	BTranslatorRoster *roster = NULL);
+	// Get default settings for the translator with the id forTranslator
+	
+static BMessage *GetDefaultSettings(const char *kTranslatorName,
+	int32 translatorVersion);
+	// Get default settings for the translator with the name kTranslatorName
+
+// Envious of that "Save As" menu in ShowImage? Well, you can have your own! */
+// AddTranslationItems will add menu items for all translations from the */
+// basic format you specify (B_TRANSLATOR_BITMAP, B_TRANSLATOR_TEXT etc). */
+// The translator ID and format constant chosen will be added to the message */
+// that is sent to you when the menu item is selected. */
+	enum {
+		B_TRANSLATION_MENU = 'BTMN'
+	};
+static status_t AddTranslationItems(BMenu *intoMenu, uint32 fromType,
+	const BMessage *kModel = NULL,	/* default B_TRANSLATION_MENU */
+	const char *kTranslatorIdName = NULL, /* default "be:translator" */
+	const char *kTranslatorTypeName = NULL, /* default "be:type" */
+	BTranslatorRoster *roster = NULL);
 				
 // BEGIN: Added by Michael Wilber
 private:
 
-		/* Translates the image data from pio to the type type using the */
-		/* supplied BTranslatorRoster. If BTranslatorRoster is not supplied */
-		/* the default BTranslatorRoster is used. */
-static BBitmap * TranslateToBitmap(
-				BPositionIO * pio,
-				uint32 type,
-				BTranslatorRoster * use = NULL);
+static BBitmap * TranslateToBitmap(BPositionIO *pio, uint32 type,
+	BTranslatorRoster *roster = NULL);
+	// Translates the image data from pio to the type type using the
+	// supplied BTranslatorRoster. If BTranslatorRoster is not supplied
+	// the default BTranslatorRoster is used.
+	
 // END: Added by Michael Wilber
 				
 };
