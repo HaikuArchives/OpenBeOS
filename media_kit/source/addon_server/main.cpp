f78086335ff075a0a002826689305fbd271d975a
beveloper <>
2002-05-03 13:39:15 +0000

    a working media_addon_server

--- media_kit/source/addon_server/main.cpp
+++ media_kit/source/addon_server/main.cpp
@@ -10,10 +10,13 @@
 #include <Path.h>
 #include <Directory.h>
 #include <Looper.h>
+#define DEBUG 1
+#include <Debug.h>
+#include "debug.h"
 #include "TMap.h"
 #include "../server/headers/ServerInterface.h"
 
-media_addon_id nextid = 1;
+void DumpFlavorInfo(const flavor_info *info);
 
 class Application : BApplication
 {
@@ -28,6 +31,8 @@ public:
 	void AddOnRemoved(ino_t file_node);
 	void HandleMessage(int32 code, void *data, size_t size);
 	static int32 controlthread(void *arg);
+	
+	void ScanAddOnFlavors(BMediaAddOn *addon);
 
 	Map<ino_t,media_addon_id> *filemap;
 	Map<media_addon_id,BMediaAddOn *> *addonmap;
@@ -77,6 +82,19 @@ Application::HandleMessage(int32 code, void *data, size_t size)
 			write_port(msg->reply_port, 0, &reply, sizeof(reply));
 			break;
 		}
+
+		case ADDONSERVER_RESCAN_MEDIAADDON_FLAVORS:
+		{
+			const xfer_addonserver_rescan_mediaaddon_flavors *msg = (const xfer_addonserver_rescan_mediaaddon_flavors *)data;
+			BMediaAddOn *addon;
+			if (addonmap->Get(msg->addonid, &addon)) {
+				ASSERT(msg->addonid == addon->AddonID());
+				ScanAddOnFlavors(addon);		
+			} else {
+				printf("Can't find a addon object for id %d\n",(int)msg->addonid);
+			} 
+			break;
+		}
 	}
 }
 
@@ -112,12 +130,80 @@ Application::ReadyToRun()
 }
 
 void 
+Application::ScanAddOnFlavors(BMediaAddOn *addon)
+{
+	int flavorcount;
+	media_addon_id purge_id;
+	port_id port;
+	status_t rv;
+	
+	ASSERT(addon);
+	
+	port = find_port("media_server port");
+	if (port <= B_OK) {
+		printf("couldn't find media_server port\n");
+		return;
+	}
+	
+	ASSERT(addon->AddonID() > 0);
+
+	// the server should remove previously registered "dormant_flavor_info"s
+	purge_id = addon->AddonID();
+
+	flavorcount = addon->CountFlavors();
+	printf("%d flavors:\n",flavorcount);
+
+	for (int i = 0; i < flavorcount; i++) {
+		const flavor_info *info;
+		printf("flavor %d:\n",i);
+		if (B_OK != addon->GetFlavorAt(i, &info)) {
+			printf("failed!\n");
+			continue;
+		}
+		
+		DumpFlavorInfo(info);
+		
+		dormant_flavor_info dfi;
+		dfi = *info;
+		dfi.node_info.addon = addon->AddonID();
+		dfi.node_info.flavor_id = info->internal_id;
+		strncpy(dfi.node_info.name, info->name, B_MEDIA_NAME_LENGTH - 1);
+		dfi.node_info.name[B_MEDIA_NAME_LENGTH - 1] = 0;
+		
+		xfer_server_register_dormant_node *msg;
+		size_t flattensize;
+		size_t msgsize;
+		
+		flattensize = dfi.FlattenedSize();
+		msgsize = flattensize + sizeof(xfer_server_register_dormant_node);
+		msg = (xfer_server_register_dormant_node *) malloc(msgsize);
+		
+		msg->purge_id = purge_id;
+		msg->dfi_type = dfi.TypeCode();
+		msg->dfi_size = flattensize;
+		dfi.Flatten(&(msg->dfi),flattensize);
+
+		rv = write_port(port, SERVER_REGISTER_DORMANT_NODE, msg, msgsize);
+		if (rv != B_OK) {
+			printf("couldn't register dormant node\n");
+		}
+
+		free(msg);
+		
+		// after the first iteration, we don't want the server to anymore remove our dfis;
+		if (purge_id != 0)
+			purge_id = 0;
+	}
+}
+
+void 
 Application::AddOnAdded(const char *path, ino_t file_node)
 {
-	printf("loading BMediaAddOn from %s\n",path);
+	printf("\n\nloading BMediaAddOn from %s\n",path);
 
 	BMediaAddOn *(*make_addon)(image_id you);
 	BMediaAddOn *addon = 0;
+	media_addon_id id;
 	image_id image;
 	status_t rv;
 	
@@ -130,57 +216,51 @@ Application::AddOnAdded(const char *path, ino_t file_node)
 	rv = get_image_symbol(image, "make_media_addon", B_SYMBOL_TYPE_TEXT, (void**)&make_addon);
 	if (rv < B_OK) {
 		printf("loading failed, function not found %lx %s\n", rv, strerror(rv));
+		unload_add_on(image);
 		return;
 	}
 	
 	addon = make_addon(image);
 	if (addon == 0) {
 		printf("creating BMediaAddOn failed\n");
-		goto finished;
+		unload_add_on(image);
+		return;
 	}
-/*	
-	const char *error;
-	rv = addon->InitCheck(&error);
-	if (rv != B_OK) {
-		printf("BMediaAddOn::InitCheck() failed, %lx %s \"%s\"\n", rv, strerror(rv), error);
-		goto finished;
-	}
-*/
-	int flavorcount;
-	flavorcount = addon->CountFlavors();
-	printf("%d flavors:\n",flavorcount);
-	for (int i = 0; i < flavorcount; i++) {
-		const flavor_info *info;
-		printf("flavor %d:\n",i);
-		if (B_OK != addon->GetFlavorAt(i, &info)) {
-			printf("failed!\n");
-			continue;
-		}
-		printf("  internal_id = %ld\n",info->internal_id);
-		printf("  name = %s\n",info->name);
-		printf("  info = %s\n",info->info);
-		printf("  flavor_flags = 0x%lx",info->flavor_flags);
-		if (info->flavor_flags & B_FLAVOR_IS_GLOBAL) printf(" B_FLAVOR_IS_GLOBAL");
-		if (info->flavor_flags & B_FLAVOR_IS_LOCAL) printf(" B_FLAVOR_IS_LOCAL");
-		printf("\n");
-		printf("  kinds = 0x%Lx",info->kinds);
-		if (info->kinds & B_BUFFER_PRODUCER) printf(" B_BUFFER_PRODUCER");
-		if (info->kinds & B_BUFFER_CONSUMER) printf(" B_BUFFER_CONSUMER");
-		if (info->kinds & B_TIME_SOURCE) printf(" B_TIME_SOURCE");
-		if (info->kinds & B_CONTROLLABLE) printf(" B_CONTROLLABLE");
-		if (info->kinds & B_FILE_INTERFACE) printf(" B_FILE_INTERFACE");
-		if (info->kinds & B_ENTITY_INTERFACE) printf(" B_ENTITY_INTERFACE");
-		if (info->kinds & B_PHYSICAL_INPUT) printf(" B_PHYSICAL_INPUT");
-		if (info->kinds & B_PHYSICAL_OUTPUT) printf(" B_PHYSICAL_OUTPUT");
-		if (info->kinds & B_SYSTEM_MIXER) printf(" B_SYSTEM_MIXER");
-		printf("\n");
-		printf("  in_format_count = %ld\n",info->in_format_count);
-		printf("  out_format_count = %ld\n",info->out_format_count);
-
-	}
-	printf("WantsAutoStart = %s\n",addon->WantsAutoStart() ? "yes" : "no");
-	printf("\n");
 	
+	id = addon->AddonID();
+	ASSERT(id > 0);
+
+	printf("adding media add-on %d\n",(int)id);
+
+	filemap->Insert(file_node, id);
+	addonmap->Insert(id, addon);
+
+	ScanAddOnFlavors(addon);
+	
+	if (addon->WantsAutoStart())
+		printf("#### WantsAutoStart!\n");
+
+/*
+ * the mixer (which we can't load because of unresolved symbols)
+ * is the only node that uses autostart (which is not implemented yet)
+ */
+
+/*
+loading BMediaAddOn from /boot/beos/system/add-ons/media/mixer.media_addon
+adding media add-on -1
+1 flavors:
+flavor 0:
+  internal_id = 0
+  name = Be Audio Mixer
+  info = The system-wide sound mixer of the future.
+  flavor_flags = 0x0
+  kinds = 0x40003 B_BUFFER_PRODUCER B_BUFFER_CONSUMER B_SYSTEM_MIXER
+  in_format_count = 1
+  out_format_count = 1
+#### WantsAutoStart!
+*/
+
+/*
 	if (addon->WantsAutoStart()) {
 		for (int32 index = 0; ;index++) {
 			BMediaNode *outNode;
@@ -203,16 +283,7 @@ Application::AddOnAdded(const char *path, ino_t file_node)
 		}
 		return;
 	}
-	
-	
-	media_addon_id id;
-	id = nextid++;
-	printf("adding media add-on %d\n",(int)id);
-	filemap->Insert(file_node, id);
-
-finished:
-	delete addon;
-	unload_add_on(image);
+*/
 }
 
 
@@ -228,7 +299,7 @@ Application::AddOnRemoved(ino_t file_node)
 	filemap->Remove(file_node);
 	printf("removing media add-on %d\n",(int)id);
 	if (!addonmap->Get(id,&addon)) {
-		printf("no BMediaAddon obejct found\n");
+		printf("no BMediaAddon object found\n");
 		return;
 	}
 	delete addon;
@@ -334,4 +405,29 @@ int main()
 	new Application("application/x-vnd.OpenBeOS-media-addon-server");
 	be_app->Run();
 	return 0;
+}
+
+void DumpFlavorInfo(const flavor_info *info)
+{
+	printf("  name = %s\n",info->name);
+	printf("  info = %s\n",info->info);
+	printf("  internal_id = %ld\n",info->internal_id);
+	printf("  possible_count = %ld\n",info->possible_count);
+	printf("  flavor_flags = 0x%lx",info->flavor_flags);
+	if (info->flavor_flags & B_FLAVOR_IS_GLOBAL) printf(" B_FLAVOR_IS_GLOBAL");
+	if (info->flavor_flags & B_FLAVOR_IS_LOCAL) printf(" B_FLAVOR_IS_LOCAL");
+	printf("\n");
+	printf("  kinds = 0x%Lx",info->kinds);
+	if (info->kinds & B_BUFFER_PRODUCER) printf(" B_BUFFER_PRODUCER");
+	if (info->kinds & B_BUFFER_CONSUMER) printf(" B_BUFFER_CONSUMER");
+	if (info->kinds & B_TIME_SOURCE) printf(" B_TIME_SOURCE");
+	if (info->kinds & B_CONTROLLABLE) printf(" B_CONTROLLABLE");
+	if (info->kinds & B_FILE_INTERFACE) printf(" B_FILE_INTERFACE");
+	if (info->kinds & B_ENTITY_INTERFACE) printf(" B_ENTITY_INTERFACE");
+	if (info->kinds & B_PHYSICAL_INPUT) printf(" B_PHYSICAL_INPUT");
+	if (info->kinds & B_PHYSICAL_OUTPUT) printf(" B_PHYSICAL_OUTPUT");
+	if (info->kinds & B_SYSTEM_MIXER) printf(" B_SYSTEM_MIXER");
+	printf("\n");
+	printf("  in_format_count = %ld\n",info->in_format_count);
+	printf("  out_format_count = %ld\n",info->out_format_count);
 }
