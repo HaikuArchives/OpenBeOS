a1ad9568a44773b00dade750dd8393283a3c3324
dreid <>
2002-05-12 01:04:59 +0000

    Wow!
    This adds...
    - new module file
    - icmp_error
    - icmp_input now handles some real cases in addition to ping!
    - icmp_send
    - icmp_reflect
    - adjusts the structure for ctlinput

---
+++ net_kit/source/server/icmp/icmp_module.h
@@ -0,0 +1,22 @@
+/* icmp_module.h
+ */
+ 
+#ifndef ICMP_MODULE_H
+#define ICMP_MODULE_H
+
+#include "net_module.h"
+
+#ifdef _KERNEL_MODE
+#include <KernelExport.h>
+#define ICMP_MODULE_PATH	"network/protocol/icmp"
+#else /* _KERNEL_MODE */
+#define ICMP_MODULE_PATH	"modules/protocol/icmp"
+#endif /* _KERNEL_MODE */
+
+struct icmp_module_info {
+	struct kernel_net_module_info info;
+
+	void (*error)(struct mbuf *, int, int, n_long, struct ifnet *);
+};
+
+#endif /* ICMP_MODULE_H */
