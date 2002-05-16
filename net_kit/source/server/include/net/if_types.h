8c0dddfde51546744a53f2485275bfdcfb4250d3
dreid <>
2002-05-16 17:19:29 +0000

    The style police have been again...

--- net_kit/source/server/include/net/if_types.h
+++ net_kit/source/server/include/net/if_types.h
@@ -8,16 +8,16 @@
 
 /* We just list the ones here we actually use... */
 
-#define IFT_ETHER	0x06
-#define IFT_PPP		0x17
-#define IFT_LOOP	0x18
-#define IFT_SLIP	0x1c
-#define IFT_RS232	0x21
-#define IFT_PARA	0x22 /* Parallel port! ?? */
-#define IFT_ATM		0x25
-#define IFT_MODEM	0x31 /* Just a simple generic modem */
-#define IFT_FASTETHER	0x32 /* 100BaseT ethernet */
-#define IFT_ISDN	0x3f /* ISDN / X.25 */
+#define IFT_ETHER         0x06
+#define IFT_PPP           0x17
+#define IFT_LOOP          0x18
+#define IFT_SLIP          0x1c
+#define IFT_RS232         0x21
+#define IFT_PARA          0x22 /* Parallel port! ?? */
+#define IFT_ATM           0x25
+#define IFT_MODEM         0x31 /* Just a simple generic modem */
+#define IFT_FASTETHER     0x32 /* 100BaseT ethernet */
+#define IFT_ISDN          0x3f /* ISDN / X.25 */
 
 #endif /* NET_IF_TYPES_H */
 
