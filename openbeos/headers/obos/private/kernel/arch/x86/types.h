d0f0f69b8e158b69fef22cd35f6bb67cae315b12
michaelphipps <>
2002-06-03 02:06:43 +0000

    Creation

---
+++ openbeos/headers/obos/private/kernel/arch/x86/types.h
@@ -0,0 +1,23 @@
+/*
+** Copyright 2001, Travis Geiselbrecht. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+#ifndef _I386_TYPES_H
+#define _I386_TYPES_H
+
+#ifndef WIN32
+typedef unsigned long long uint64;
+typedef long long           int64;
+#else
+typedef unsigned __int64   uint64;
+typedef __int64             int64;
+#endif
+typedef unsigned int       uint32;
+typedef int                 int32;
+typedef unsigned short     uint16;
+typedef short               int16;
+typedef unsigned char      uint8;
+typedef char                int8;
+typedef unsigned long addr;
+#endif
+
