c47e0c397f0544f3f4a9d34681079326ae3edd5d
bonefish <>
2002-06-09 17:41:56 +0000

    Added documentation for MIME C functions.

--- storage_kit/source/lib/Mime.cpp
+++ storage_kit/source/lib/Mime.cpp
@@ -14,6 +14,23 @@ enum {
 };
 
 // update_mime_info
+/*!	\brief Updates the MIME information for one or more files.
+	If \a path points to a file, the MIME information for this file are
+	updated only. If it points to a directory and \a recursive is non-null,
+	the information for all the files in the given directory tree are updated.
+	If path is \c NULL all files are considered; \a recursive is ignored in
+	this case.
+	\param path The path to a file or directory, or \c NULL.
+	\param recursive Non-null to trigger recursive behavior.
+	\param synchronous If non-null update_mime_info() waits until the
+		   operation is finished, otherwise it returns immediately and the
+		   update is done asynchronously.
+	\param force If non-null, also the information for files are updated that
+		   have already been updated.
+	\return
+	- \c B_OK: Everything went fine.
+	- An error code otherwise.
+*/
 int
 update_mime_info(const char *path, int recursive, int synchronous, int force)
 {
@@ -21,6 +38,21 @@ update_mime_info(const char *path, int recursive, int synchronous, int force)
 }
 
 // create_app_meta_mime
+/*!	Creates a MIME database entry for one or more applications.
+	\a path should either point to an application file or should be \c NULL.
+	In the first case a MIME database entry for that application is created,
+	in the second case entries for all applications are created.
+	\param path The path to an application file, or \c NULL.
+	\param recursive Currently unused.
+	\param synchronous If non-null create_app_meta_mime() waits until the
+		   operation is finished, otherwise it returns immediately and the
+		   operation is done asynchronously.
+	\param force If non-null, entries are created even if they do already
+		   exist.
+	\return
+	- \c B_OK: Everything went fine.
+	- An error code otherwise.
+*/
 status_t
 create_app_meta_mime(const char *path, int recursive, int synchronous,
 					 int force)
@@ -29,6 +61,15 @@ create_app_meta_mime(const char *path, int recursive, int synchronous,
 }
 
 // get_device_icon
+/*!	Retrieves an icon associated with a given device.
+	\param dev The path to the device.
+	\param icon A pointer to a buffer the icon data shall be written to.
+	\param size The size of the icon. Currently the sizes 16 (small) and
+		   32 (large) are supported.
+	\return
+	- \c B_OK: Everything went fine.
+	- An error code otherwise.
+*/
 status_t
 get_device_icon(const char *dev, void *icon, int32 size)
 {
