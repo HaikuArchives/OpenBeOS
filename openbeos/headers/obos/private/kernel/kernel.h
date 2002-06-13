0c5238db52db5144974e3691874dc63cd9f94a5d
dreid <>
2002-06-13 22:08:33 +0000

    Couldn't see any good reason for this to be in arch so move it to the top level where everything is happier about finding it.

--- openbeos/headers/obos/private/kernel/kernel.h
+++ openbeos/headers/obos/private/kernel/kernel.h
@@ -5,27 +5,13 @@
 #ifndef _KERNEL_ARCH_KERNEL_H
 #define _KERNEL_ARCH_KERNEL_H
 
-#ifdef ARCH_x86
-#include <arch/x86/kernel.h>
-#endif
-#ifdef ARCH_sh4
-#include <arch/sh4/kernel.h>
-#endif
-#ifdef ARCH_alpha
-#include <arch/alpha/kernel.h>
-#endif
-#ifdef ARCH_sparc64
-#include <arch/sparc64/kernel.h>
-#endif
-#ifdef ARCH_mips
-#include <arch/mips/kernel.h>
-#endif
-#ifdef ARCH_ppc
-#include <arch/ppc/kernel.h>
-#endif
-#ifdef ARCH_m68k
-#include <arch/m68k/kernel.h>
-#endif
+/* david - same fiddle as stage2.h. Renamed the arch specific
+ * kernel.h file into arch_kernel.h and then just include
+ * it here as the arch specific directories are included on the
+ * include paths for the build.
+ */
+ 
+#include <arch_kernel.h>
 
 #define KSTACK_SIZE (PAGE_SIZE*2)
 #define STACK_SIZE  (PAGE_SIZE*16)
