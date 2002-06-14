bb91eb579ade7e1db5fe163f6966988ddc7f7cfc
dreid <>
2002-06-14 12:26:22 +0000

    Add a lot of new (and temporary) files. These are being included here so we can build and run the kernel as close to newos as possible. Eventually these will be moved/replaced.

---
+++ openbeos/sources/os/kits/kernel/apps/ls/main.c
@@ -0,0 +1,106 @@
+/*
+** Copyright 2002, Travis Geiselbrecht. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+#include <syscalls.h>
+#include <stdio.h>
+#include <string.h>
+
+void (*disp_func)(const char *, struct file_stat *) = NULL;
+
+static void display_l(const char *filename, struct file_stat *stat)
+{
+	const char *type;
+
+	switch(stat->type) {
+		case STREAM_TYPE_FILE:
+			type = "FILE";
+			break;
+		case STREAM_TYPE_DEVICE:
+			type = "DEV ";
+			break;
+		case STREAM_TYPE_DIR:
+			type = "DIR ";
+			break;
+		default:
+			type = "UNKN";
+	}
+
+	printf("%s %12Ld %s\n", type, stat->size, filename);
+}
+
+static void display(const char *filename, struct file_stat *stat)
+{
+	printf("%s\n", filename);
+}
+
+int main(int argc, char *argv[])
+{
+	int rc;
+	int rc2;
+	int count = 0;
+	struct file_stat stat;
+	char *arg;
+
+	if(argc == 1) {
+		arg = ".";
+	} else {
+		arg = argv[1];
+	}
+
+	// for now, use the '-l' version of the display func
+	disp_func = display_l;
+
+	rc = sys_rstat(arg, &stat);
+	if(rc < 0) {
+		printf("sys_rstat() returned error: %s!\n", strerror(rc));
+		goto err_ls;
+	}
+
+	switch(stat.type) {
+		case STREAM_TYPE_DIR: {
+			int fd;
+			char buf[1024];
+			int len;
+
+			fd = sys_open(arg, STREAM_TYPE_DIR, 0);
+			if(fd < 0) {
+				printf("ls: sys_open() returned error: %s!\n", strerror(fd));
+				break;
+			}
+
+			for(;;) {
+				char buf2[1024];
+
+				rc = sys_read(fd, buf, -1, sizeof(buf));
+				if(rc <= 0)
+					break;
+
+				buf2[0] = 0;
+				if(strcmp(arg, ".") != 0) {
+					strlcpy(buf2, arg, sizeof(buf2));
+					strlcat(buf2, "/", sizeof(buf2));
+				}
+				strlcat(buf2, buf, sizeof(buf2));
+
+				rc2 = sys_rstat(buf2, &stat);
+				if(rc2 >= 0) {
+					(*disp_func)(buf2, &stat);
+				}
+				count++;
+			}
+			sys_close(fd);
+
+			printf("%d files found\n", count);
+			break;
+		}
+		default:
+			(*disp_func)(argv[1], &stat);
+			break;
+	}
+
+err_ls:
+	return 0;
+}
+
+
---
+++ openbeos/sources/os/kits/kernel/apps/shell/main.c
@@ -0,0 +1,79 @@
+/*
+** Copyright 2001, Travis Geiselbrecht. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+#include <string.h>
+#include <ctype.h>
+#include <syscalls.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "commands.h"
+#include "parse.h"
+#include "statements.h"
+#include "shell_defs.h"
+#include "shell_vars.h"
+#include "args.h"
+
+static int readline(char *buf, int len)
+{
+	int i = 0;
+	char ch;
+
+	while(true){
+
+		ch = getchar();
+
+		switch(ch){
+		case  8 :
+			if(i>0){
+				printf("%c", ch);
+				i --;
+			}
+			break;
+
+		case '\n':
+			buf[i] = 0;
+			printf("\n");
+			return i;
+
+		default:
+			buf[i] = ch;
+			printf("%c",ch);
+			i++;
+		}
+	}
+}
+
+int main(int argc,char *argv[])
+{
+	char buf[1024];
+
+	init_vars();
+	init_statements();
+	init_arguments(argc,argv);
+
+	if(af_script_file_name != NULL){
+		run_script(af_script_file_name);
+		if(af_exit_after_script) sys_exit(0);
+	}
+
+	printf("Welcome to the NewOS shell\n");
+
+	for(;;) {
+		int chars_read;
+
+		printf("> ");
+
+		chars_read = readline(buf, sizeof(buf));
+ 		if(chars_read > 0) {
+			parse_string(buf);
+		}
+	}
+
+	printf("shell exiting\n");
+
+	return 0;
+}
+
+
