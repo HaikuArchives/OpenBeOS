87edb4edea65cda8957fd995fa10ae52d872df48
dreid <>
2002-06-15 13:09:41 +0000

    Add missing files so we have all the functions we need to avoid unresolved symbols :)

---
+++ openbeos/sources/os/kits/kernel/core/addons/fs/zfs/zfs_dir.c
@@ -0,0 +1,14 @@
+/*
+** Copyright 2002, Travis Geiselbrecht. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+#include <kernel.h>
+#include <errors.h>
+
+#include <zfs.h>
+
+int zfs_lookup(fs_cookie fs, fs_vnode dir, const char *name, vnode_id *id)
+{
+	return ERR_NOT_IMPLEMENTED_YET;
+}
+
