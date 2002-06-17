6bd36427b25d741ea299cf40149bd3738f93bde9
ejakowatz <>
2002-06-17 18:32:32 +0000

    Moved from app_kit/test/lib/application.

---
+++ openbeos/sources/tests/kits/app/BMessageQueue/AddMessageTest2.h
@@ -0,0 +1,33 @@
+/*
+	$Id$
+	
+	This file defines a class for performing one test of BMessageQueue
+	functionality.
+	
+	*/
+
+
+#ifndef AddMessageTest2_H
+#define AddMessageTest2_H
+
+
+#include "MessageQueueTestCase.h"
+#include "Test.h"
+#include "ThreadedTestCaller.h"
+
+	
+template<class MessageQueue> class AddMessageTest2 :
+	public MessageQueueTestCase<MessageQueue> {
+	
+private:
+	typedef ThreadedTestCaller <AddMessageTest2<MessageQueue> >
+		AddMessageTest2Caller;
+	
+public:
+	static Test *suite(void);
+	virtual void PerformTest(void);
+	AddMessageTest2(std::string);
+	virtual ~AddMessageTest2();
+	};
+	
+#endif
