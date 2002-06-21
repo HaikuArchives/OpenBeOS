04f83766df344aa4fb6ecb8a2b045c01cb731e97
dreid <>
2002-06-21 01:16:26 +0000

    This is virtually all the POSIX error codes. where we need to we maintain Be compatibility. Next step is to get Errors.h using this and then we'll have a full set across C and C++ with common numbering as far as possible :)
    Also, visit strerror and add in descriptions for these. Are we concerned
    about locales at present or is that R2?

--- openbeos/headers/obos/private/kernel/errno.h
+++ openbeos/headers/obos/private/kernel/errno.h
@@ -1,40 +1,148 @@
 /*
-** Copyright 2002, Travis Geiselbrecht. All rights reserved.
-** Copyright 2002, Manuel J. Petit. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * errno.h
+ *
+ * POSIX style error codes.
+ * Be values used where required to keep compatibility.
+ */
 
-#ifndef _newos__errno__hh_
-#define _newos__errno__hh_
-
+#ifndef _POSIX_ERRNO_H
+#define _POSIX_ERRNO_H
 
 #ifdef __cplusplus
 extern "C"
 {
+#endif  
+
+/* XXX - this really belongs in limits.h, but as we don't have one
+ * yet it's here so we can build.
+ * XXX - move me!
+ */
+/* Minimum's */
+#ifndef LONG_MIN
+#define LONG_MIN          (-2147483647L-1)
 #endif
 
-
-/*
- * this will change when we get TLS working
- */
+/* XXX - Fix this once TLS works */
 extern int errno;
 
+/* We're defining values based on the values that Be used, hence
+ * the starting point at + 0x7000
+ */
+#define POSIX_ERROR_START    LONG_MIN + 0x7000
 
+/* The basic error codes that don't have a B_ equivalent
+ *
+ * NB when adding codes make sure that the Be Errors.h didn't
+ *    define a value or it needs to go in the bottom section and
+ *    have an identical value assigned.
+ * NB new values just go on the bottom.
+ */
+enum {
+	E2BIG = POSIX_ERROR_START + 1,
+	ECHILD,
+	EDEADLK,
+	EFBIG,
+	EMLINK,
+	ENFILE,
+	ENODEV,
+	ENOLCK,
+	ENOSYS,
+	ENOTTY,              /* +10 */
+	ENXIO,
+	ESPIPE,
+	ESRCH,
+	EFPOS,
+	ESIGPARM,
+	EDOM,
+	ERANGE,
+	EPROTOTYPE,
+	EPROTONOSUPPORT,
+	EPFNOSUPPORT,        /* +20 */
+	EAFNOSUPPORT,
+	EADDRINUSE,
+	EADDRNOTAVAIL,
+	ENETDOWN,
+	ENETUNREACH,
+	ENETRESET,
+	ECONNABORTED,
+	ECONNRESET,
+	EISCONN,
+	ENOTCONN,            /* +30 */
+	ESHUTDOWN,
+	ECONNREFUSED,
+	EHOSTUNREACH,
+	ENOPROTOOPT,
+	ENOBUFS,
+	EINPROGRESS,
+	EALREADY,
+	EILSEQ,
+	ENOMSG,
+	ESTALE,              /* +40 */
+	EOVERFLOW,
+	EMSGSIZE,
+	EOPNOTSUPP,
+	ENOTSOCK,
+	
+	/* These are additional to the Be ones */
+	EBADMSG,
+	ECANCELLED,
+	EDESTADDRREQ,
+	EDQUOT,
+	EHOSTDOWN,
+	EIDRM,               /* +50 */
+	EISDIR,
+	EMULTIHOP,
+	ENODATA,
+	ENOLINK,
+	ENOSR,
+	ENOSTR,
+	EPROTO,
+	EPROTOOPT,
+	ETIME,
+};
 
 /*
- * These need to match system errors... will do some other day,
- * right now I'm just happy getting stdio to compile
+ * Errors with Be equivalents
  */
-/* XXX - Replace with Be values - david has a file ready for this :) */
-#define ENOMEM	    0xF0000000
-#define EBADF	    0xF0000001
-#define EINVAL	    0xF0000002
-#define EPERM       0xF0000003
-#define EOPNOTSUPP  0xF0000004
+
+/* TODO: add comments showing what the equivalent Be errors are... */
+
+/* NB - These have strange values to be the same as the Be values... */
+
+/* General errors */
+enum {
+	ENOMEM = LONG_MIN,
+	EIO,
+	EACCES,
+	EINVAL = LONG_MIN + 5,
+	ETIMEDOUT = LONG_MIN + 9,
+	EINTR,
+	EWOULDBLOCK,
+	EAGAIN = EWOULDBLOCK,
+	EBUSY = LONG_MIN + 14,
+	EPERM
+};
+
+/* Storage kit/File system errors */
+enum {
+	EBADF = LONG_MIN + 0x6000,
+	EEXIST = EBADF + 2,
+	ENOENT,
+	ENAMETOOLONG,
+	ENOTDIR,
+	ENOTEMPTY,
+	ENOSPC,
+	EROFS,
+	EMFILE,
+	EXDEV,
+	ELOOP,
+	EPIPE
+};
+
+#define ENOEXEC   LONG_MIN + 0x1302
 
 #ifdef __cplusplus
 } /* "C" */
-#endif
+#endif 
 
-
-#endif
+#endif /* _POSIX_ERRNO_H */
