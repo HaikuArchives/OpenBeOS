223df6ff2a83715a4d31494e9bc93f1f5a2db818
dreid <>
2002-06-21 00:09:20 +0000

    Hmm, we can't really include Errors.h as we don't have such a file in our OpenBeOS tree and we shouldn't be using Be files, so just use errors.h and there will shortly be a commit that adds the needed codes there.

--- openbeos/sources/os/kits/kernel/core/vm/vm.c
+++ openbeos/sources/os/kits/kernel/core/vm/vm.c
@@ -2,7 +2,7 @@
 ** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
 ** Distributed under the terms of the NewOS License.
 */
-#include <Errors.h>
+#include <errors.h>
 #include <kernel.h>
 #include <vm.h>
 #include <vm_priv.h>
