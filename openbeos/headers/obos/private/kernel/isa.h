f397acd6ed62b83edf3d75174fd838c39b1d812a
dreid <>
2002-06-28 11:18:57 +0000

    Bring the headers in line with the changes about to hit the module code. Basically we now have Be like syntax for dealing with kernel modules, right down to the structures and functions required.

--- openbeos/headers/obos/private/kernel/isa.h
+++ openbeos/headers/obos/private/kernel/isa.h
@@ -8,14 +8,17 @@
 #ifndef __ISA_H__
 #define __ISA_H__
 
+#include <module.h>
+
 #define ISA_MODULE_NAME "bus_managers/isa/v1"
 
 typedef struct {
-	uint8			(*read_io_8) (int mapped_io_addr);
-	void			(*write_io_8) (int mapped_io_addr, uint8 value);
-	uint16			(*read_io_16) (int mapped_io_addr);
+	module_info     info;
+	uint8			(*read_io_8)   (int mapped_io_addr);
+	void			(*write_io_8)  (int mapped_io_addr, uint8 value);
+	uint16			(*read_io_16)  (int mapped_io_addr);
 	void			(*write_io_16) (int mapped_io_addr, uint16 value);
-	uint32			(*read_io_32) (int mapped_io_addr);
+	uint32			(*read_io_32)  (int mapped_io_addr);
 	void			(*write_io_32) (int mapped_io_addr, uint32 value);
 } isa_bus_manager;
 
--- openbeos/headers/obos/private/kernel/module.h
+++ openbeos/headers/obos/private/kernel/module.h
@@ -9,45 +9,6 @@
 #include <kernel.h>
 #include <stage2.h>
 
-#define MODULE_CURR_VERSION 0x10000
-// if set, module gets neither uninit'd nor unloaded
-#define MODULE_KEEP_LOADED 1
-
-// user function list and management function lists are separated
-struct module_header {
-	const char *name;				// with path (for clients convinience)
-	uint32 version;
-	uint32 flags;
-	void *interface;				// pointer to function list
-	
-	int ( *init )( void );
-	int ( *uninit )( void );
-};
-
-typedef struct modules_cookie {} *modules_cookie;
-
-typedef struct module_header module_header;
-
-// to become a module, export this symbol
-// it has to be a null-terminated list of pointers to module headers
-
-extern module_header *newos_modules[];
-
-// flags must be 0
-extern int module_get( const char *name, int flags, void **interface );
-extern int module_put( const char *name );
-
-extern modules_cookie module_open_list( const char *prefix );
-//extern int read_next_module_name(void *cookie, char *buf, size_t *bufsize );
-//extern int close_module_list(void *cookie );
-
-
-// boot init
-// XXX this is very private, so move it away
-extern int module_init( kernel_args *ka, module_header **sys_module_headers );
-
-/* Be Module Compatability... */
-
 typedef struct module_info module_info;
 
 struct module_info {
@@ -55,6 +16,12 @@ struct module_info {
 	uint32      flags;
 	int32       (*std_ops)(int32, ...);
 };
+
+// boot init
+// XXX this is very private, so move it away
+extern int module_init( kernel_args *ka, module_info **sys_module_headers );
+
+/* Be Module Compatability... */
 
 #define	B_MODULE_INIT	1
 #define	B_MODULE_UNINIT	2
@@ -70,4 +37,4 @@ int  close_module_list(void *cookie);
 
 extern module_info *modules[];
 
-#endif
+#endif /* _|KRENEL_MODULE_H */
