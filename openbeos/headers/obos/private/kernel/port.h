1ab60dd9bd8ab83f3a26f9f70fe133ad8d03bffd
dreid <>
2002-06-30 01:14:28 +0000

    Change the port calls to match beos!

--- openbeos/headers/obos/private/kernel/port.h
+++ openbeos/headers/obos/private/kernel/port.h
@@ -21,41 +21,46 @@
 int port_init(kernel_args *ka);
 
 // kernel API
-port_id		port_create(int32 queue_length, const char *name);
-int			port_close(port_id id);
-int			port_delete(port_id id);
-port_id		port_find(const char *port_name);
-int			port_get_info(port_id id, struct port_info *info);
-int		 	port_get_next_port_info(proc_id proc,
-				uint32 *cookie,
-				struct port_info *info);
+port_id		create_port(int32 queue_length, const char *name);
+int			close_port(port_id id);
+int			delete_port(port_id id);
+port_id		find_port(const char *port_name);
+int			_get_port_info(port_id id, struct port_info *info, size_t);
+int		 	_get_next_port_info(proc_id proc, uint32 *, 
+                                struct port_info *, size_t);
 ssize_t		port_buffer_size(port_id port);
 ssize_t		port_buffer_size_etc(port_id port,
 				uint32 flags,
 				bigtime_t timeout);
 int32		port_count(port_id port);
-ssize_t		port_read(port_id port,
+ssize_t		read_port(port_id port,
 				int32 *msg_code,
 				void *msg_buffer,
 				size_t buffer_size);
-ssize_t		port_read_etc(port_id port,
+ssize_t		read_port_etc(port_id port,
 				int32 *msg_code,
 				void *msg_buffer,
 				size_t buffer_size,
 				uint32 flags,
 				bigtime_t timeout);
-int			port_set_owner(port_id port, proc_id proc);
-int			port_write(port_id port,
+int			set_port_owner(port_id port, proc_id proc);
+int			write_port(port_id port,
 				int32 msg_code,
 				void *msg_buffer,
 				size_t buffer_size);
-int		 	port_write_etc(port_id port,
+int		 	write_port_etc(port_id port,
 				int32 msg_code,
 				void *msg_buffer,
 				size_t buffer_size,
 				uint32 flags,
 				bigtime_t timeout);
-int port_delete_owned_ports(proc_id owner);
+int delete_owned_ports(proc_id owner);
+
+#define get_port_info(port, info)    \
+             _get_port_info((port), (info), sizeof(*(info)))
+	
+#define get_next_port_info(team, cookie, info)   \
+	         _get_next_port_info((team), (cookie), (info), sizeof(*(info)))
 
 
 // temp: test
@@ -63,26 +68,26 @@ void port_test(void);
 int	 port_test_thread_func(void* arg);
 
 // user-level API
-port_id		user_port_create(int32 queue_length, const char *name);
-int			user_port_close(port_id id);
-int			user_port_delete(port_id id);
-port_id		user_port_find(const char *port_name);
-int			user_port_get_info(port_id id, struct port_info *info);
-int		 	user_port_get_next_port_info(proc_id proc,
+port_id		user_create_port(int32 queue_length, const char *name);
+int			user_close_port(port_id id);
+int			user_delete_port(port_id id);
+port_id		user_find_port(const char *port_name);
+int			user_get_port_info(port_id id, struct port_info *info);
+int		 	user_get_next_port_info(proc_id proc,
 				uint32 *cookie,
 				struct port_info *info);
 ssize_t		user_port_buffer_size_etc(port_id port,
 				uint32 flags,
 				bigtime_t timeout);
 int32		user_port_count(port_id port);
-ssize_t		user_port_read_etc(port_id port,
+ssize_t		user_read_port_etc(port_id port,
 				int32 *msg_code,
 				void *msg_buffer,
 				size_t buffer_size,
 				uint32 flags,
 				bigtime_t timeout);
-int			user_port_set_owner(port_id port, proc_id proc);
-int			user_port_write_etc(port_id port,
+int			user_set_port_owner(port_id port, proc_id proc);
+int			user_write_port_etc(port_id port,
 				int32 msg_code,
 				void *msg_buffer,
 				size_t buffer_size,
